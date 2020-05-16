a8a5cf596f89b8d5d4f8cd6b1228e98904cd64f3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-05-16 21:16:49 +0100

    Added script to redo platepar creation/star alignment

---
+++ DailyChecks/redoPlatepar.ps1
@@ -0,0 +1,19 @@
+# a script for doing Pi Camera astrometry with the latest set of data
+#
+#remember where we were
+$here=get-location 
+# read the inifile
+set-location $PSScriptRoot
+$ini=get-content camera1.ini -raw | convertfrom-stringdata
+$RMSloc=$ini.rms_loc
+
+# find the latest set of data on the local drive
+$destpath=$ini.localfolder+'\ArchivedFiles'
+$path=(get-childitem $destpath -directory | sort-object creationtime | select-object -last 1).name
+$myf = $destpath + '\'+$path
+
+set-location $rmsloc
+python -m RMS.Astrometry.SkyFit --config . $myf
+
+#go back to where we were
+set-location $here
