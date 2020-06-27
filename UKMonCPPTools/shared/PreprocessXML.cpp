908d6231ac81ea59adb2614291c4d8f6d43a63bb
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-06-27 19:26:23 +0100

    Added back the icon to ukmonlivecl

--- UKMonCPPTools/shared/PreprocessXML.cpp
+++ UKMonCPPTools/shared/PreprocessXML.cpp
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include "..\LiveUploader\UKMonLiveCL.h"
+#include "..\ukmonlive\UKMonLiveCL.h"
 #include "..\shared\tinyxml\tinyxml.h"
 #include "..\shared\llsq.h"
 
