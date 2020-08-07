cf71731f826e36a0b2ccc6ef0fa5d3cbf12b3ba0
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-08-07 10:33:51 +0100

    fixed inifile handling

--- DailyChecks/redoPlatepar.ps1
+++ DailyChecks/redoPlatepar.ps1
@@ -4,15 +4,24 @@
 $here=get-location 
 # read the inifile
 set-location $PSScriptRoot
+# load the helper functions
+. helperfunctions.ps1
+# read the inifile
 if ($args.count -eq 0) {
-    $ini=get-content camera1.ini -raw | convertfrom-stringdata
-}else {
-    $ini=get-content $args[0] -raw | convertfrom-stringdata
+    $inifname='../TACKLEY_TC.ini'
 }
-$RMSloc=$ini.rms_loc
+else {
+    $inifname = $args[0]
+}
+$ini=get-inicontent $inifname
+$RMSLoc=$ini['rms']['rms_loc']
+$localfolder=$ini['camera']['localfolder']
+$RMSenv=$ini['rms']['rms_env']
+
+conda activate $RMSenv
 
 # find the latest set of data on the local drive
-$destpath=$ini.localfolder+'\ArchivedFiles'
+$destpath=$localfolder+'\ArchivedFiles'
 $path=(get-childitem $destpath -directory | sort-object creationtime | select-object -last 1).name
 $myf = $destpath + '\'+$path
 
