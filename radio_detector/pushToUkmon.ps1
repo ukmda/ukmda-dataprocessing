2aeb37a6207c2cca12c5aeb40aec14175b5b1f71
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-08-18 22:32:56 +0100

    added logging

--- radio_detector/pushToUkmon.ps1
+++ radio_detector/pushToUkmon.ps1
@@ -16,7 +16,7 @@ if((test-path $inifname) -eq $false){
     write-output "datadir missing or invalid, can't continue"
     exit 2
 }
-
+$logf=$datadir+'/logs/ukmon-'+(get-date -uformat '%Y%m%d')+'.log'
 $ukmonkey=$ini['ukmon']['ukmon_keyfile']
 $keys=((Get-Content $ukmonkey)[1]).split(',')
 $Env:AWS_ACCESS_KEY_ID = $keys[0]
@@ -24,6 +24,7 @@ $env:AWS_SECRET_ACCESS_KEY = $keys[1]
 
 $station=$ini['ukmon']['ukmon_station']
 $srcloc=$datadir+'\csv\'
+write-output 'updating ukmon' | tee-object $logf 
 $s3targ='s3://ukmon-shared/archive/' + $station + '/Radio/'
-aws s3 sync $srcloc $s3targ
+aws s3 sync $srcloc $s3targ | tee-object $logf -append
 set-location $PSScriptRoot
