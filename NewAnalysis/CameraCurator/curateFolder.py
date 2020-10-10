908d7e990cc569fe1736d20a403bf156c65232f6
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-10-10 19:00:01 +0100

    Add ability to override source and badfile path

--- NewAnalysis/CameraCurator/curateFolder.py
+++ NewAnalysis/CameraCurator/curateFolder.py
@@ -11,7 +11,7 @@ from CameraCurator import curateCamera as cc
 from CameraCurator import curateEngine as ce
 
 
-def main(path):
+def main(path, altfolder=None, badfolder=None):
     config = cfg.ConfigParser()
 
     # if the argument is an ini file, read from it.
@@ -23,32 +23,37 @@ def main(path):
     else:
         cfgfile = os.path.join(path, 'testing.ini')
         config.read(cfgfile)
-
     cc.badfilepath = config['cleaning']['badfolder']
+
+    # we can override the ini file by passing in an alternate folder
+    if altfolder is not None:
+        path = altfolder
+    if badfolder is not None:
+        cc.badfilepath = badfolder
+
     logfilepath = cc.badfilepath
     ce.MAXRMS = float(config['cleaning']['maxrms'])
     ce.MINLEN = int(config['cleaning']['minlen'])
     ce.MAXLEN = int(config['cleaning']['maxlen'])
     ce.MAXBRI = int(config['cleaning']['maxbri'])
     ce.MAXOBJS = int(config['cleaning']['maxobjs'])
-    print(config['cleaning']['debug'])
     if config['cleaning']['debug'] in ['True', 'TRUE', 'true']:
         ce.debug = True
     else:
         ce.debug = False
     if config['cleaning']['movefiles'] in ['True', 'TRUE', 'true']:
-        cc.movfiles = True
+        movfiles = True
     else:
-        cc.movfiles = False
+        movfiles = False
 
     if config['cleaning']['useSubfolders'] in ['True', 'TRUE', 'true']:
-        cc.useSubfolders = True
+        useSubfolders = True
     else:
-        cc.useSubfolders = False
+        useSubfolders = False
 
-    print('Processing ' + path + '; cc.movefiles=' + str(cc.movfiles))
+    print('Processing ' + path + '; cc.movefiles=' + str(movfiles))
 
-    cc.ProcessADay(path, '*', cc.badfilepath, logfilepath)
+    cc.ProcessADay(path, '*', cc.badfilepath, logfilepath, movfiles, useSubfolders)
 
 
 if __name__ == '__main__':
