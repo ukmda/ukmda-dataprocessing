d86c7cf03a8d45b203c6546785292b96bfff6ab1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-10-28 20:15:55 +0000

    update analysis scripts

--- Reports/analyse.sh
+++ Reports/analyse.sh
@@ -4,9 +4,14 @@ if [ $# -lt 2 ] ; then
 	echo Usage: analyse.sh GEM 2017
 else
 	./GENERATE_REPORTS_V2.r $1 $2
+	echo .
+	echo done generating reports, now copying them
 	if [ -d REPORTS/$2/$1 ] ; then 
 		mkdir -p ~/data/analysis/$2/$1
 		cp REPORTS/$2/$1/* ~/data/analysis/$2/$1
+	else
+		echo nothing to copy - no output created!
 	fi
+	echo done
 fi
 
