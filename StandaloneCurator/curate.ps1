254140915b925640d4cc35bd3313be69c3035676
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-11-30 15:36:40 +0000

    improvements to installation instructions and process

--- StandaloneCurator/curate.ps1
+++ StandaloneCurator/curate.ps1
@@ -8,12 +8,14 @@ if ($args.count -lt 1 ){
     Write-Output "will curate all UFO files under c:\ufodata\2020"
     exit
 }
+if ( !(test-path "logs")) {mkdir logs}
+if ( (test-path "~\ufoCurator\scripts")){ ~\ufoCurator\scripts\activate.ps1 }
+
 if ( !(test-path ".installed"))
 {
     pip install -r requirements.txt
     Write-Output '1' > .installed
 }
-if ( !(test-path "logs")) {mkdir logs}
 
 $logf='./logs/curate-'+(get-date -uformat '%Y%m%d_%H%M%S')+'.log'
 python ./CurateUFO.py ./curation.ini $args[0] | tee-object $logf
