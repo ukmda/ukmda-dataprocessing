98a1084937c6f90782aaa36ddbf182ed8932f2b3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-01-21 20:35:14 +0000

    updating filenames for deployment

--- archive/website/deploy-site.ps1
+++ archive/website/deploy-site.ps1
@@ -4,10 +4,10 @@
 
 ~/scripts/set-aws-creds.ps1 mark-creds
 
-get-content template-header.html, template-mainpage.html, template-footer.html | set-content index.html
-get-content template-header.html, template-about.html, template-footer.html | set-content about.html
+get-content header.template, mainpage.template, footer.template | set-content index.html
+get-content header.template, about.template, footer.template | set-content about.html
 
-aws s3 sync . s3://mjmm-ukmonarchive.co.uk --exclude "template*"  --exclude "deploy*"
+aws s3 sync . s3://mjmm-ukmonarchive.co.uk --exclude "*.template"  --exclude "deploy*"
 
 Remove-Item .\index.html
 Remove-Item .\about.html
