718ec626f42af4c292640d461e217e2927e19598
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-02-15 22:02:53 +0000

    Added ability to invoke API gateway and retrieve data

--- archive/website/static_content/data/searchdialog.js
+++ archive/website/static_content/data/searchdialog.js
@@ -35,3 +35,35 @@ $('#selectInterval').on('rangeSelected', function(){
     }
 });
       
+var apiurl = 'https://0zbnc358p0.execute-api.eu-west-1.amazonaws.com/test';
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  //console.log("Saving value", form.elements.value.value);
+  var d1 = document.getElementById("datestart").innerHTML;
+  var a = Date.parse(d1);
+  var d2 = document.getElementById("dateend").innerHTML;
+  var b = Date.parse(d2);
+  var op = "foo";
+  //jQuery.support.cors = true;
+  payload = {"a":  a, "b": b, "op":  op };
+  console.log(payload);
+  $.ajax({
+    url: apiurl, 
+    type: "GET",
+    data: payload,
+    dataType: 'jsonp',
+    complete: function (xhr, status, ex) {
+      if (status === 'error' ) {
+        alert(status + "," + ex );
+      }
+    }
+  });
+  event.preventDefault();}
+  );
+function myFunc(myObj) {
+    var x, txt = "";
+    for (x in myObj) {
+      txt += myObj[x].name + "<br>";
+    }
+    document.getElementById("dateend").innerHTML = txt;
+}
