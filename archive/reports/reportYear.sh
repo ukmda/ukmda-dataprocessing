b020d1c1bfbfabe3d474ad9c1cb557efde8a2b39
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-04-22 22:34:02 +0100

    Automate shower reports using IMO list to decide dates to run for

--- archive/reports/reportYear.sh
+++ archive/reports/reportYear.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# monthlyReports collects the data for the given year
-# so the other reports can be run
-./monthlyReports.sh ALL $1 $2
-./createReport.sh QUA $1 $2
-./createReport.sh LYR $1 $2
-./createReport.sh ETA $1 $2
-./createReport.sh SDA $1 $2
-./createReport.sh CAP $1 $2
-./createReport.sh PER $1 $2
-./createReport.sh AUR $1 $2
-./createReport.sh SPE $1 $2
-./createReport.sh OCT $1 $2
-./createReport.sh DRA $1 $2
-./createReport.sh EGE $1 $2
-./createReport.sh ORI $1 $2
-./createReport.sh STA $1 $2
-./createReport.sh NTA $1 $2
-./createReport.sh LEO $1 $2
-./createReport.sh MON $1 $2
-./createReport.sh GEM $1 $2
-./createReport.sh URS $1 $2
+here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source $here/../config/config.ini >/dev/null 2>&1
+source ~/venvs/$RMS_ENV/bin/activate
+
+export PYTHONPATH=$RMS_LOC:$PYLIB
+
+if [ $# -eq 0 ]; then
+    yr=$(date +%Y)
+else
+    ym=$1
+fi
+
+# get list of active showers
+python $PYLIB/utils/getActiveShowers.py | while read i 
+do 
+    echo "processing $i for $yr"
+    $SRC/analysis/createReport.sh $i $yr force
+done
---
+++ ukmon_pylib/fileformats/imoWorkingShowerList.py
@@ -0,0 +1,91 @@
+#
+# python module to read the IMO Working Shower short List
+#
+
+import xmltodict
+import datetime
+
+majorlist=['QUA','LYR','ETA','SDA','CAP','PER','AUR','SPE','OCT','DRA',
+    'EGE','ORI','STA','NTA','LEO','MON','GEM','URS']
+
+
+class IMOshowerList:
+
+    def __init__(self, fname):
+        with open(fname) as fd:
+            tmplist = xmltodict.parse(fd.read())
+            self.showerlist = tmplist['meteor_shower_list']['shower']
+
+    def getShowerByCode(self, iaucode):
+        for shower in self.showerlist:
+            if shower['IAU_code'] == iaucode:
+                return shower
+
+    def getStart(self, iaucode, yr=None):
+        shower = self.getShowerByCode(iaucode)
+        now = datetime.datetime.today().year
+        if yr is not None:
+            now = yr
+        startdate = datetime.datetime.strptime(shower['start'], '%b %d')
+        startdate = startdate.replace(year=now)
+        return startdate
+
+    def getEnd(self, iaucode, yr=None):
+        shower = self.getShowerByCode(iaucode)
+        now = datetime.datetime.today().year
+        if yr is not None:
+            now = yr
+        if iaucode == 'QUA':
+            now = now + 1
+        enddate = datetime.datetime.strptime(shower['end'], '%b %d')
+        enddate = enddate.replace(year=now)
+        return enddate
+
+    def getPeak(self, iaucode, yr=None):
+        shower = self.getShowerByCode(iaucode)
+        now = datetime.datetime.today().year
+        if yr is not None:
+            now = yr
+        if iaucode == 'QUA':
+            now = now + 1
+        enddate = datetime.datetime.strptime(shower['peak'], '%b %d')
+        enddate = enddate.replace(year=now)
+        return enddate
+
+    def getRvalue(self, iaucode):
+        shower = self.getShowerByCode(iaucode)
+        return shower['r']
+
+    def getName(self, iaucode):
+        shower = self.getShowerByCode(iaucode)
+        return shower['name']
+
+    def getVelocity(self, iaucode):
+        shower = self.getShowerByCode(iaucode)
+        return shower['V']
+
+    def getZHR(self, iaucode):
+        shower = self.getShowerByCode(iaucode)
+        zhr = shower['ZHR']
+        if zhr is None:
+            return -1
+        else:
+            return int(zhr)
+
+    def getRaDec(self, iaucode):
+        shower = self.getShowerByCode(iaucode)
+        return float(shower['RA']), float(shower['DE'])
+
+    def getActiveShowers(self, datetotest, majonly=False):
+        activelist = []
+        for shower in self.showerlist:
+            shwname = shower['IAU_code']
+            yr = datetotest.year
+            if shwname == 'QUA':
+                yr = yr-1
+            start = self.getStart(shwname, yr)
+            end = self.getEnd(shwname, yr) + datetime.timedelta(days=1)
+            if datetotest > start and datetotest < end:
+                if majonly is False or (majonly is True and shwname in majorlist):
+                    activelist.append(shwname)
+        return activelist
