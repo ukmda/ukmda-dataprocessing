f6a7975b6ce92207781256a9686ecf9c4b701d2f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-04-30 23:37:29 +0100

    Support for a gallery of images

--- archive/website/createPageIndex.sh
+++ archive/website/createPageIndex.sh
@@ -29,40 +29,41 @@ pref=${repfile:0:16}
 fldr=$(basename $datadir)
 
 cp $TEMPLATES/header.html $idxfile
-echo "<div class=\"container\">" >> $idxfile
 echo "<h2>Orbital Analysis for matched events on $ym</h2>" >> $idxfile
 echo "<pre>" >> $idxfile
 cat ${datadir}/summary.html >> $idxfile
 echo "Click <a href=\"./$fldr.zip\">here</a> to download a zip of the raw and processed data." >> $idxfile
 echo "</pre>" >>$idxfile
+echo "<p><b>Detailed report below graphs</b></p>" >>$idxfile
 echo "<h3>Click on an image to see a larger view</h3>" >> $idxfile
 
+echo "<div class=\"top-img-container\">" >> $idxfile
 echo "<a href=\"${pref}orbit_top.png\"><img src=\"${pref}orbit_top.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}orbit_side.png\"><img src=\"${pref}orbit_side.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}ground_track.png\"><img src=\"${pref}ground_track.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}velocities.png\"><img src=\"${pref}velocities.png\" width=\"20%\"></a>" >> $idxfile
 echo "<br>">>$idxfile
-
 ls -1 ${datadir}/*.jpg | while read jpg 
 do
     jpgbn=`basename $jpg`
     echo "<a href=\"$jpgbn\"><img src=\"$jpgbn\" width=\"20%\"></a>" >> $idxfile
 done
-echo "<br>">>$idxfile
-
-echo "<p>More graphs below the detailed report<br></p>" >>$idxfile
-echo "<pre>" >>$idxfile 
-cat $repf >>$idxfile
-echo "</pre>" >>$idxfile
 echo "<a href=\"${pref}lengths.png\"><img src=\"${pref}lengths.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}lags_all.png\"><img src=\"${pref}lags_all.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}abs_mag.png\"><img src=\"${pref}abs_mag.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}abs_mag_ht.png\"><img src=\"${pref}abs_mag_ht.png\" width=\"20%\"></a>" >> $idxfile
-echo "<br>">>$idxfile
-#echo "<a href=\"${pref}all_spatial_residuals.png\"><img src=\"${pref}all_spatial_residuals.png\" width=\"20%\"></a>" >> $idxfile
-#echo "<a href=\"${pref}total_spatial_residuals_length_grav.png\"><img src=\"${pref}total_spatial_residuals_length_grav.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}all_angular_residuals.png\"><img src=\"${pref}all_angular_residuals.png\" width=\"20%\"></a>" >> $idxfile
 echo "<a href=\"${pref}all_spatial_total_residuals_height.png\"><img src=\"${pref}all_spatial_total_residuals_height.png\" width=\"20%\"></a>" >> $idxfile
+echo "</div>" >> $idxfile
+
+echo "<pre>" >>$idxfile 
+cat $repf >>$idxfile
+echo "</pre>" >>$idxfile
+echo "<br>">>$idxfile
+
+echo "<script> \$('.top-img-container').magnificPopup({ " >> $idxfile
+echo "delegate: 'a', type: 'image', image:{verticalFit:false}, gallery:{enabled:true} }); " >> $idxfile
+echo "</script>" >> $idxfile
 
 cat $TEMPLATES/footer.html >> $idxfile
 
