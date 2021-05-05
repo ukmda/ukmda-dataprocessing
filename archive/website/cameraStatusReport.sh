1fa5ffb7d589017763a26eedba7523d7a10aa2a1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-05-05 23:39:58 +0100

    Added camera status report

---
+++ archive/website/cameraStatusReport.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create report of camera statuses
+
+here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# load the configuration
+source $here/../config/config.ini >/dev/null 2>&1
+
+export PYTHONPATH=$PYLIB
+cp $TEMPLATES/header.html /tmp/statrep.html
+python $PYLIB/reports/cameraStatusReport.py $MATCHDIR/RMSCorrelate >> /tmp/statrep.html
+cat $TEMPLATES/footer.html >> /tmp/statrep.html
+
+source $WEBSITEKEY
+aws s3 cp /tmp/statrep.html $WEBSITEBUCKET/reports/
