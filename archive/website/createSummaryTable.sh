df1c336d73c3ff0e4b24d6fac97007f928b7a930
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-05-14 23:35:03 +0100

    pythonified the summary table

--- archive/website/createSummaryTable.sh
+++ archive/website/createSummaryTable.sh
@@ -3,51 +3,14 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 source $here/../config/config.ini >/dev/null 2>&1
 
-logger -s -t createSummaryTable "starting"
+logger -s -t createSummaryTable "creating summary table"
 cd $here/../analysis
-echo "\$(function() {" > $here/data/summarytable.js
-echo "var table = document.createElement(\"table\");" >> $here/data/summarytable.js
-echo "table.className = \"table table-striped table-bordered table-hover table-condensed\";" >> $here/data/summarytable.js
-echo "var header = table.createTHead();" >> $here/data/summarytable.js
-echo "header.className = \"h4\";" >> $here/data/summarytable.js
 
+export PYTHONPATH=$PYLIB
+export SRC 
+export DATADIR
 yr=$(date +%Y)
-until [ $yr -lt 2013 ]; do
-    if [ $yr -gt 2019 ] ; then 
-        detections=$(grep "OTHER Matched" $SRC/logs/ALL$yr.log | awk '{print $4}')
-    else
-        detections=`cat $DATADIR/consolidated/M_${yr}-unified.csv | wc -l`
-    fi
-    matches=`grep "UNIFIED Matched" $SRC/logs/ALL${yr}.log  | awk '{print $4}'`
-    fireballs=`tail -n +2 $DATADIR/reports/$yr/ALL/TABLE_Fireballs.csv |wc -l`
-    echo "var row = table.insertRow(-1);" >> $here/data/summarytable.js
-    echo "var cell = row.insertCell(0);" >> $here/data/summarytable.js
-    echo "cell.innerHTML = \"<a href="/reports/$yr/ALL/index.html">$yr</a>\";" >> $here/data/summarytable.js
-    echo "var cell = row.insertCell(1);" >> $here/data/summarytable.js
-    echo "cell.innerHTML = \"$detections\";" >> $here/data/summarytable.js
-    echo "var cell = row.insertCell(2);" >> $here/data/summarytable.js
-    echo "cell.innerHTML = \"<a href="/reports/$yr/orbits/index.html">$matches</a>\";" >> $here/data/summarytable.js
-    echo "var cell = row.insertCell(3);" >> $here/data/summarytable.js
-    echo "cell.innerHTML = \"$fireballs\";" >> $here/data/summarytable.js
-    ((yr=yr-1))
-done
-echo "var row = header.insertRow(0);" >> $here/data/summarytable.js
-echo "var cell = row.insertCell(0);" >> $here/data/summarytable.js
-echo "cell.innerHTML = \"Year\";" >> $here/data/summarytable.js
-echo "cell.className = \"small\";" >> $here/data/summarytable.js
-echo "var cell = row.insertCell(1);" >> $here/data/summarytable.js
-echo "cell.innerHTML = \"Detections\";" >> $here/data/summarytable.js
-echo "cell.className = \"small\";" >> $here/data/summarytable.js
-echo "var cell = row.insertCell(2);" >> $here/data/summarytable.js
-echo "cell.innerHTML = \"Matches\";" >> $here/data/summarytable.js
-echo "cell.className = \"small\";" >> $here/data/summarytable.js
-echo "var cell = row.insertCell(3);" >> $here/data/summarytable.js
-echo "cell.innerHTML = \"Fireballs\";" >> $here/data/summarytable.js
-echo "cell.className = \"small\";" >> $here/data/summarytable.js
-
-echo "var outer_div = document.getElementById(\"summarytable\");" >> $here/data/summarytable.js
-echo "outer_div.appendChild(table);" >> $here/data/summarytable.js
-echo "})" >> $here/data/summarytable.js
+python $PYLIB/reports/createSummaryTable.py $here/data/summarytable.js $yr
 
 logger -s -t createSummaryTable "create a coverage map from the kmls"
 source ~/venvs/${RMS_ENV}/bin/activate
---
+++ ukmon_pylib/reports/createSummaryTable.py
@@ -0,0 +1,84 @@
+#
+# Python module to create the archive website summary table for the front page
+#
+import sys
+import os
+import subprocess
+
+
+def createSummaryTable(fname, curryr):
+    """Creates a summary of all data for the website front page. The table has four columns,
+    the year, number of detections, number of matches and number of fireballs. 
+
+    Args:
+        fname (str): full path to the output file
+        curryr (str): current year
+
+    """
+    with open(fname, 'w') as f:
+        f.write('$(function() {\n')
+        f.write('var table = document.createElement("table");\n')
+        f.write('table.className = "table table-striped table-bordered table-hover table-condensed";\n')
+        f.write('var header = table.createTHead();\n')
+        f.write('header.className = "h4";\n')
+
+        srcpath = os.getenv('SRC')
+        datadir = os.getenv('DATADIR')
+        for yr in range(int(curryr), 2012, -1):
+
+            if yr > 2019:
+                srchfile = os.path.join(srcpath, 'logs', 'ALL{}.log'.format(yr))
+                cmd = ['grep', 'OTHER Matched', srchfile]
+                dets = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+                detections = dets.split(' ')[3]
+            else:
+                srchfile = os.path.join(datadir, 'consolidated', 'M_{}-unified.csv'.format(yr))
+                cmd = ['wc', '-l', srchfile]
+                dets = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+                detections = dets.split(' ')[0]
+
+            srchfile = os.path.join(srcpath, 'logs', 'ALL{}.log'.format(yr))
+            cmd = ['grep', 'UNIFIED Matched', srchfile]
+            dets = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+            matches = dets.split(' ')[3]
+
+            srchfile = os.path.join(datadir, 'reports', '{}'.format(yr), 'ALL','TABLE_Fireballs.csv')
+            cmd = ['wc', '-l', srchfile]
+            dets = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+            if len(dets) > 0:
+                fireballs = dets.split(' ')[0]
+                fireballs = int(fireballs)-1
+            else:
+                fireballs = 0
+
+            f.write('var row = table.insertRow(-1);\n')
+            f.write('var cell = row.insertCell(0);\n')
+            f.write('cell.innerHTML = "<a href=/reports/{}/ALL/index.html>{}</a>";\n'.format(yr, yr))
+            f.write('var cell = row.insertCell(1);\n')
+            f.write('cell.innerHTML = "{}";\n'.format(detections))
+            f.write('var cell = row.insertCell(2);\n')
+            f.write('cell.innerHTML = "<a href=/reports/{}/orbits/index.html>{}</a>";\n'.format(yr, matches))
+            f.write('var cell = row.insertCell(3);\n')
+            f.write('cell.innerHTML = "{}";\n'.format(fireballs))
+
+        f.write('var row = header.insertRow(0);\n')
+        f.write('var cell = row.insertCell(0);\n')
+        f.write('cell.innerHTML = "Year";\n')
+        f.write('cell.className = "small";\n')
+        f.write('var cell = row.insertCell(1);\n')
+        f.write('cell.innerHTML = "Detections";\n')
+        f.write('cell.className = "small";\n')
+        f.write('var cell = row.insertCell(2);\n')
+        f.write('cell.innerHTML = "Matches";\n')
+        f.write('cell.className = "small";\n')
+        f.write('var cell = row.insertCell(3);\n')
+        f.write('cell.innerHTML = "Fireballs";\n')
+        f.write('cell.className = "small";\n')
+
+        f.write('var outer_div = document.getElementById("summarytable");\n')
+        f.write('outer_div.appendChild(table);\n')
+        f.write('})\n')
+
+
+if __name__ == '__main__':
+    createSummaryTable(sys.argv[1], sys.argv[2])
