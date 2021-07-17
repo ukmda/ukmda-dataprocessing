264aba4f99c1d9062dc90ef5b88cef6392b3a79a
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-07-17 11:59:49 +0100

    Added station reports

--- archive/analysis/stationReports.sh
+++ archive/analysis/stationReports.sh
@@ -13,6 +13,14 @@ cd $RCODEDIR
 ./STATION_SUMMARY_MASTER.r $yr
 
 source $WEBSITEKEY
-aws s3 sync $SRC/data/reports/stations/  s3://ukmeteornetworkarchive/reports/stations/ --quiet
+aws s3 sync $SRC/data/reports/stations/  $WEBSITEBUCKET/reports/stations/ --quiet
 logger -s -t stationReports "shower report done"
+
+logger -s -t stationReports "list of connected stations"
+sudo grep publickey /var/log/secure | grep -v ec2-user | grep "$(date "+%b %d")" | awk '{printf("%s, %s\n", $3,$9)}' > /tmp/stationlogins.txt
+source $WEBSITEKEY
+aws s3 cp /tmp/stationlogins.txt $WEBSITEBUCKET/reports/stationlogins.txt
+
+logger -s -t stationReports "finished"
+
 
