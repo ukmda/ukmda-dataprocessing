1875c19996d91c874bb31a0fe2f1ad782fc657b4
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-08-07 21:19:23 +0100

    bugfix in bar chart

--- ukmon_pylib/reports/createAnnualBarChart.py
+++ ukmon_pylib/reports/createAnnualBarChart.py
@@ -13,7 +13,7 @@ from wmpl.Utils.TrajConversions import jd2Date
 def createBarChart(fname):
     matches = uof.MatchedCsv(fname)
     v1=int(matches.rawdata['_mjd'][0])
-    v2=int(matches.rawdata['_mjd'][len(matches.rawdata)-1])
+    v2=int(matches.rawdata['_mjd'][len(matches.rawdata)-1])+2
     ranges=list(range(v1,v2))
     li=matches.rawdata.groupby(pd.cut(matches.rawdata['_mjd'], ranges)).count()['_mjd'].tolist()
 
