fdd900ce1fc0fe47e40d93a63972769774e4ae92
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-10-01 23:24:58 +0100

    enable retrieval of historical metrics

--- archive/costs/getMetrics.ps1
+++ archive/costs/getMetrics.ps1
@@ -7,6 +7,10 @@ conda activate RMS
 $env:PYTHONPATH="$wmpl_loc;$PYLIB"
 $env:AWS_DEFAULT_REGION="eu-west-1"
 
-
 . ~/.ssh/mark-creds.ps1
-python $PYLIB/metrics/getMetrics.py . eu-west-1
+if ($args.count -eq 0) {
+    python $PYLIB/metrics/getMetrics.py . eu-west-1
+}else{
+    python $PYLIB/metrics/getMetrics.py . eu-west-1 $args[0]
+}
+
