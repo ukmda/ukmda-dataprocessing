a8f0b6a05c22cae29f15e15cf9eb727aa53eab02
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-10-08 17:46:46 +0100

    performance improvements

--- archive/cronjobs/updateSearchIndex.sh
+++ archive/cronjobs/updateSearchIndex.sh
@@ -6,9 +6,29 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 source $here/../config/config.ini >/dev/null 2>&1
 
+wait=1
+while [ $wait -eq 1 ] ; do 
+    if [ ! -f $SRC/data/.nightly_running ] ; then  
+        wait=0
+    else
+        echo "nightly job running, waiting for it to finish"
+        sleep 300
+    fi
+done 
+
 source $WEBSITEKEY
 logger -s -t updateSearchIndex "getting latest livefeed CSV files"
-aws s3 sync s3://ukmon-live/ ${DATADIR}/ukmonlive/ --exclude "*" --include "*.csv" --quiet
+yr=$(date +%Y)
+mth=$(date +%m)
+cq=$(((mth - 1 ) / 3 + 1))
+lq=$(((mth - 1 ) / 3 ))
+aws s3 cp s3://ukmon-live/idx${yr}0${cq}.csv ${DATADIR}/ukmonlive/
+aws s3 cp s3://ukmon-live/idx${yr}0${lq}.csv ${DATADIR}/ukmonlive/
+
+# aws s3 sync s3://ukmon-live/ ${DATADIR}/ukmonlive/ --exclude "*" --include "*.csv" --quiet
+
+logger -s -t updateSearchIndex "getting list of single jpg files"
+aws s3 ls $WEBSITEBUCKET/img/single/$yr/ --recursive | awk '{print $4}' > /tmp/single.csv
 
 logger -s -t updateSearchIndex "creating searchable indices"
 $SRC/analysis/createSearchable.sh
