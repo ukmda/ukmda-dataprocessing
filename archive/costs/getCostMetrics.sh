fdea3278c52a3c9c36363c43f24237c7a259a654
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-10-19 10:59:48 +0100

    rename to make purpose clearer

--- archive/costs/getCostMetrics.sh
+++ archive/costs/getCostMetrics.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
-# script to get AWS logs for ukmon data
+# script to get AWS cost metrics for ukmon tagged assets
 #
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/../config/config.ini >/dev/null 2>&1
 
-thismth=`date '+%Y%m'`
-thisyr=`date '+%Y'`
+if [ $# -lt 1 ] ; then 
+    thismth=`date '+%m'`
+else
+    thismth=$1
+fi
 
 source /home/ec2-user/venvs/${RMS_ENV}/bin/activate
 export PYTHONPATH=$wmpl_loc:$PYLIB
@@ -15,5 +18,4 @@ export AWS_DEFAULT_REGION=eu-west-1
 cd $SRC/metrics
 
 source ~/.ssh/marks-keys
-python $PYLIB/metrics/getMetrics.py $here eu-west-1
-#python $PYLIB/metrics/getMetrics.py $here eu-west-2
+python $PYLIB/metrics/costMetrics.py $here eu-west-1 $thismth
