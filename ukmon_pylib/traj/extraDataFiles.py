d96c1bf377f48bbb9c0a86d5cc5321db0193f7b5
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-11-08 20:26:07 +0000

    more support for videos on the website

--- ukmon_pylib/traj/extraDataFiles.py
+++ ukmon_pylib/traj/extraDataFiles.py
@@ -4,21 +4,24 @@
 import os
 import sys
 import glob
+import shutil
 
 from wmpl.Utils.Pickling import loadPickle 
 from wmpl.Utils.TrajConversions import jd2Date
 from datetime import datetime, timedelta
 from traj.ufoTrajSolver import createAdditionalOutput, calcAdditionalValues, loadMagData
+from fileformats import CameraDetails as cdet
 
 
 def generateExtraFiles(outdir):
-
+    outdir=os.path.normpath(outdir)
     picklefile = glob.glob1(outdir, '*.pickle')[0]
     traj = loadPickle(outdir, picklefile)
     traj.save_results = True
 
     createAdditionalOutput(traj, outdir)
     findMatchingJpgs(traj, outdir)
+    findMatchingMp4s(traj, outdir)
     return
 
 
@@ -84,6 +87,81 @@ def findMatchingJpgs(traj, outdir):
                     outf.write('{}\n'.format(mtch[0]))
             else: 
                 outf.write('{}\n'.format(mtch[0]))
+
+
+def findMatchingMp4s(traj, outdir):
+    archdir = os.getenv('ARCHDIR')
+    if archdir is None:
+        archdir='/home/ec2-user/ukmon-shared/archive'
+
+    print('getting camera details file')
+    cinfo = cdet.SiteInfo()
+
+    for obs in traj.observations:
+        statid = obs.station_id
+        fldr = cinfo.getFolder(statid)
+        #print(statid, fldr)
+        evtdate = jd2Date(obs.jdt_ref, dt_obj=True)
+
+        #print('station {} event {} '.format(statid, evtdate.strftime('%Y%m%d-%H%M%S')))
+        # if the event is after midnight the folder will have the previous days date
+        if evtdate.hour < 12:
+            evtdate += timedelta(days=-1)
+        yr = evtdate.year
+        ym = evtdate.year * 100 + evtdate.month
+        ymd = ym *100 + evtdate.day
+
+        #print('getting mp4s')
+        thispth = '{:s}/{:04d}/{:06d}/{:08d}/'.format(fldr, yr, ym, ymd)
+        #print(thispth)
+        srcpath = os.path.join(archdir, thispth)
+        flist = glob.glob1(srcpath, 'FF*.mp4')
+        srcfil = None
+        for fil in flist:
+            spls = fil.split('_')
+            fdt = datetime.strptime(spls[2] + '_' + spls[3],'%Y%m%d_%H%M%S')
+            tdiff = (evtdate - fdt).seconds
+            if tdiff > 0 and tdiff < 11:
+                srcfil = fil
+                break
+        if srcfil is not None:
+            srcfil = srcpath + srcfil
+            try: 
+                shutil.copy2(srcfil, outdir)
+            except FileNotFoundError:
+                pass
+        else:
+            pass
+            #print('no mp4s in {}'.format(srcpath))
+
+        print('R90 CSV, KML and FTPDetect file')
+        try:
+            flist = os.listdir(srcpath)
+            for fil in flist:
+                file_name, file_ext = os.path.splitext(fil)
+                if ('FTPdetectinfo' in fil) and (file_ext == '.txt') and ('_original' not in file_name) and ('_uncal' not in file_name) and ('_backup' not in file_name):
+                    srcfil = srcpath + fil
+                    shutil.copy2(srcfil, outdir)
+                elif file_ext == '.csv':
+                    srcfil = srcpath + fil
+                    shutil.copy2(srcfil, outdir)
+                elif file_ext == '.kml':
+                    srcfil = srcpath + fil
+                    shutil.copy2(srcfil, outdir)
+                    kmldir = os.path.join(archdir, 'kmls')
+                    shutil.copy2(srcfil, kmldir)
+        except:
+            continue
+    mp4s=glob.glob1(outdir, "*.mp4")
+    if len(mp4s) > 0:
+        _, orbdir = os.path.split(outdir)
+        #print(outdir, orbdir)
+        fullpth='reports/{}/orbits/{}/{}/{}'.format(orbdir[:4], orbdir[:6], orbdir[:8], orbdir)
+        with open(os.path.join(outdir, 'jpgs.lst'), 'a') as outf:
+            for mp4 in mp4s:
+                outf.write('{}/{}\n'.format(fullpth, mp4))
+
+    return
 
 
 if __name__ == '__main__':
