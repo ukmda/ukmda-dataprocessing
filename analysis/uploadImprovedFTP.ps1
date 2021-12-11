4f593788f2f1d3d209daf1b3fc4061a6c4db15c3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-12-11 13:13:03 +0000

    Upload revised ftpdetect file

--- analysis/uploadImprovedFTP.ps1
+++ analysis/uploadImprovedFTP.ps1
@@ -1,12 +1,13 @@
 #
 # powershell script to upload improved fit details of fireballs
 #
-# args : arg1 date, arg2 stationid
+# args : arg1 full path to ftpdetect file
 $loc = Get-Location
 set-location $PSScriptRoot
 # load the helper functions
 . .\helperfunctions.ps1
 $ini=get-inicontent analysis.ini
+Set-Location $Loc
 
 $stationdetails=$ini['fireballs']['stationdets'] 
 
@@ -22,6 +23,6 @@ $yr=$dt.substring(0,4)
 $ym=$dt.substring(0,6)
 $ymd=$dt
 
-#scp ukmonhelper:ukmon-shared/archive/$stn/$cam/$yr/$ym/$ymd/platepars_all_recalibrated.json $targpth
-Write-Output scp $targpth/$ftpname ukmonhelper:ukmon-shared/archive/$stn/$cam/$yr/$ym/$ymd/ 
-Set-Location $Loc
+Write-Output "Target location is $stn/$cam/$yr/$ym/$ymd/"
+#scp $targpth/platepars_all_recalibrated.json ukmonhelper:ukmon-shared/archive/$stn/$cam/$yr/$ym/$ymd/
+scp $targpth/$ftpname ukmonhelper:ukmon-shared/archive/$stn/$cam/$yr/$ym/$ymd/ 
