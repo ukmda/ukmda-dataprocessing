b66403a3a48a6150339c9dc49381155b93693129
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-12-13 17:32:44 +0000

    ongoing work to send emails for bad or missing cameras

--- ukmon_pylib/reports/reportBadCameras.py
+++ ukmon_pylib/reports/reportBadCameras.py
@@ -8,6 +8,8 @@ import pandas as pd
 import datetime
 import boto3
 
+from utils import sendAnEmail
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -19,6 +21,7 @@ if __name__ == '__main__':
         print('define DATADIR first')
         exit(1)
     
+    mailmsg = ''
     camowners = pd.read_csv(os.path.join(datadir, 'admin', 'stationdetails.csv'))
     camowners = camowners.rename(columns={'camid':'StationID'})
     dts = pd.read_csv(os.path.join(datadir,'latest','uploadtimes.csv'), index_col=False)
@@ -28,27 +31,36 @@ if __name__ == '__main__':
 
     latecams = dts[dts.ts.dt.date == targdate]
     if len(latecams) > 0:
-        print('Not Uploaded for {} days'.format(sys.argv[1]))
-        print('=======================')
+        print('\nNot Uploaded for {} days'.format(sys.argv[1]))
+        print('=========================')
+        mailmsg = mailmsg + '\nNot Uploaded for {} days\n'.format(sys.argv[1])
+        mailmsg = mailmsg + '=======================\n'
         latecams = pd.merge(latecams, camowners, on=['StationID'])
         latecams = latecams.drop(columns=['site'])
         print(latecams)
+        for _, row in latecams.iterrows():
+            mailmsg = mailmsg + '{} {} {}\n'.format(row['StationID'], row['ts'], row['eMail'])
 
     longerdt = int(sys.argv[1])+7
     targdate=datetime.date.today() + datetime.timedelta(days=-longerdt)
 
-    latecams = dts[dts.ts.dt.date < targdate]
+    latecams = dts[dts.ts.dt.date == targdate]
     if len(latecams) > 0:
-        print('Not Uploaded for more than {} days'.format(longerdt))
-        print('==================================')
+        print('\nNot Uploaded for {} days'.format(longerdt))
+        print('============================')
+        mailmsg = mailmsg + '\nNot Uploaded for {} days\n'.format(longerdt)
+        mailmsg = mailmsg + '============================\n'
         latecams = pd.merge(latecams, camowners, on=['StationID'])
         latecams = latecams.drop(columns=['site'])
         print(latecams)
+        for _, row in latecams.iterrows():
+            mailmsg = mailmsg + '{} {} {}\n'.format(row['StationID'], row['ts'], row['eMail'])
 
     logcli = boto3.client('logs', region_name='eu-west-2')
 
-    uxt = datetime.datetime.now() + datetime.timedelta(days=-2)
+    uxt = datetime.datetime.now() + datetime.timedelta(days=-1)
     uxt = uxt.replace(hour=0, minute=0, second=0, microsecond=0)
+    print(uxt)
     uxt = int(uxt.timestamp()*1000)
 
     badcams=[]
@@ -86,10 +98,19 @@ if __name__ == '__main__':
         if 'nextToken' not in response:
             break
     if len(badcams) > 0: 
-        print('')
-        print('Too many detections')
-        print('===================')
+        print('\nToo many detections\n===================')
+        mailmsg = mailmsg + '\nToo many detections\n'
+        mailmsg = mailmsg + '===================\n'
         badcams = pd.DataFrame(badcams, columns=['StationID','ts','reccount'])
         badcams = pd.merge(badcams, camowners, on=['StationID'])
         badcams = badcams.drop(columns=['site'])
         print(badcams)
+        for _, row in badcams.iterrows():
+            mailmsg = mailmsg + '{} {} {} {}\n'.format(row['StationID'], row['ts'], row['reccount'], row['eMail'])
+
+    mailrecip = 'markmcintyre99@googlemail.com'
+    msgtype='Late or Misbehaving camera report'
+    
+    if len(mailmsg) > 0:
+        sendAnEmail.sendAnEmail(mailrecip, mailmsg, msgtype)
+        print('mail sent')
