f7dc9a740a3bc317bb901213c4dbe18deefb8254
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-02-10 23:04:08 +0000

    upload parquet version of single-station data

--- archive/analysis/getRMSSingleData.sh
+++ archive/analysis/getRMSSingleData.sh
@@ -37,6 +37,8 @@ done
 
 # push to S3 bucket for future use by AWS tools
 source $UKMONSHAREDKEY
-aws s3 sync $SRC/data/single/ $UKMONSHAREDBUCKET/matches/single/ --exclude "*" --include "single*" --quiet
+python -m converters.toParquet $SRC/data/single/singles-${yr}.csv
+aws s3 sync $SRC/data/single/ $UKMONSHAREDBUCKET/matches/single/ --exclude "*" --include "*.csv" --quiet
+aws s3 sync $SRC/data/single/ $UKMONSHAREDBUCKET/matches/singlepq/ --exclude "*" --include "*.parquet.gzip" --quiet
 
 logger -s -t getRMSSingleData "finished"
