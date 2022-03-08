1de41b2b05d91ad16f163ee1be0eeecb1865344e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-03-08 21:09:26 +0000

    improvements to log sharing

--- archive/analysis/getLogData.sh
+++ archive/analysis/getLogData.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-lastlog=$(ls -1tr $SRC/logs/nightly-*.log | tail -1)
+here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source $here/../config/config.ini >/dev/null 2>&1
+source $HOME/venvs/$WMPL_ENV/bin/activate
+
+lastlog=$(ls -1tr $SRC/logs/nightly*.log | tail -1)
 lastmtch=$(ls -1tr $SRC/logs/matches-*.log | tail -1)
 
 egrep "nightlyJob|consolidateOutput|createMthlyExtracts|createShwrExtracts|createSearchable|createFireballPage|showerReport|reportYear|createSummaryTable|createLatestTable|stationReports" $lastlog > /tmp/logmsgs.txt
 egrep "findAllMatches1|convertUfoToRms|getRMSSingleData|runMatching" $lastmtch > /tmp/matchpt1.txt
 egrep "findAllMatches2|updateIdexPages|createOrbitIndex" $lastmtch > /tmp/matchpt2.txt
 
+
 cp $TEMPLATES/header.html $DATADIR/lastlog.html
+echo "<h2>Batch Job Report</h2>" >> $DATADIR/lastlog.html
 echo "<pre>" >> $DATADIR/lastlog.html
 head -2 /tmp/logmsgs.txt >> $DATADIR/lastlog.html
-cat /tmp/matchpt1 >> $DATADIR/lastlog.html
-python -m reports.getSolutionStati $lastmtch >> $DATADIR/lastlog.html
-cat /tmp/matchpt2 >> $DATADIR/lastlog.html
+cat /tmp/matchpt1.txt >> $DATADIR/lastlog.html
+cat /tmp/matchpt2.txt >> $DATADIR/lastlog.html
 len=$(wc -l /tmp/logmsgs.txt|awk '{print $1}')
 len=$((len-2))
 tail -$len /tmp/logmsgs.txt >> $DATADIR/lastlog.html
 echo "</pre>" >> $DATADIR/lastlog.html
+echo "<h2>Correlator Report</h2>" >> $DATADIR/lastlog.html
+echo "<pre>" >> $DATADIR/lastlog.html
+python -m reports.getSolutionStati $lastmtch >> $DATADIR/lastlog.html
+echo "</pre>" >> $DATADIR/lastlog.html
+echo "<h2>Uncalibrated Data Report</h2>" >> $DATADIR/lastlog.html
+echo "<pre>" >> $DATADIR/lastlog.html
+grep "Skipping" $lastmtch >> $DATADIR/lastlog.html
+echo "</pre>" >> $DATADIR/lastlog.html
+
 cat $TEMPLATES/footer.html >> $DATADIR/lastlog.html
-rm /tmp/matchpt1.txt /tmp/matchpt2 /tmp/logmsgs.txt
+rm /tmp/matchpt1.txt /tmp/matchpt2.txt /tmp/logmsgs.txt
+
+source $WEBSITEKEY
+aws s3 cp $DATADIR/lastlog.html  $WEBSITEBUCKET/reports/ --quiet
