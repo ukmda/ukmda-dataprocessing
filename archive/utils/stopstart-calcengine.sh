1730c013378e2e8ec2e47bce9f756932d4590860
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-03-15 00:07:56 +0000

    make it easier to stop start

--- archive/utils/stopstart-calcengine.sh
+++ archive/utils/stopstart-calcengine.sh
@@ -7,5 +7,5 @@ source $here/../config/config.ini >/dev/null 2>&1
 source /home/ec2-user/venvs/${RMS_ENV}/bin/activate
 source $SERVERAWSKEYS
 AWS_DEFAULT_REGION=eu-west-2
-aws ec2 $1 --instance-ids $SERVERINSTANCEID
+aws ec2 $1-instances --instance-ids $SERVERINSTANCEID
 aws ec2 describe-instances --instance-ids $SERVERINSTANCEID --query Reservations[*].Instances[*].PrivateIpAddress --output text
