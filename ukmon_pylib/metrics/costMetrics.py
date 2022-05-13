aab63d3d1dc3e17af2d9c1cdd1fd9df6fa113c24
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-05-13 18:01:29 +0100

    improved cost metrics

--- ukmon_pylib/metrics/costMetrics.py
+++ ukmon_pylib/metrics/costMetrics.py
@@ -1,100 +1,140 @@
 import sys
 import os
 import datetime
-import json 
 import boto3
 from botocore.config import Config
 import matplotlib.pyplot as plt
 import numpy as np
 from dateutil import relativedelta
 
-PRICEPERGBSEC = 0.00001667
+csvdtype = np.dtype([('dt', 'U10'), ('service','U64'), ('tag', 'U32'), ('cost', '<f8')])
 
-csvdtype = np.dtype([('dt', 'S10'), ('tag', 'S32'), ('cost', '<f8'), ('amt', '<f8')])
+
+def getSvcName(svc):
+    if 'Elastic Compute' in svc:
+        svcname='Compute'
+    elif 'EC2 - Other' in svc:
+        svcname='Network'
+    elif 'Glue' in svc:
+        svcname='Glue'
+    elif 'Cloudtrail' in svc:
+        svcname='Cloudtral'
+    elif 'Storage' in svc:
+        svcname='S3'
+    elif 'Container Service' in svc:
+        svcname = 'ECS'
+    elif 'Route 53' in svc:
+        svcname = 'R-53'
+    elif 'Backup' in svc:
+        svcname = 'Backup'
+    elif 'Lambda' in svc:
+        svcname = 'Lambda'
+    else:
+        svcname = 'Other'
+    return svcname
 
 
 def drawBarChart(outdir, accountid, regionid, mthdate):
-    fldr = os.path.join(outdir, '{}'.format(accountid), regionid)
-    fname = os.path.join(fldr, '{}-{}.csv'.format('ec2', mthdate))
-    ec2data = np.genfromtxt(fname, delimiter=',', dtype=csvdtype)
-    fltr=np.logical_or(ec2data['tag']==b'billingtag$ukmon',ec2data['tag']==b'billingtag$ukmonstuff')
-    ec2fdata = ec2data[fltr]
-    labels = ec2fdata['dt']
-    ec2cost = ec2fdata['cost']
-    s3cost = np.zeros(len(ec2cost))
+    fldr = os.path.join(outdir, f'{accountid}',f'{regionid}')
+    fname = os.path.join(fldr, f'costs-{mthdate}.csv')
+    costdata = np.genfromtxt(fname, delimiter=',', dtype=csvdtype, skip_header=1)
 
-    fname = os.path.join(fldr, '{}-{}.csv'.format('s3', mthdate))
-    s3data = np.genfromtxt(fname, delimiter=',', dtype=csvdtype)
-    fltr=np.logical_or(s3data['tag']==b'billingtag$ukmon',s3data['tag']==b'billingtag$ukmonstuff')
-    s3fdata = s3data[fltr]
-    s3labels = s3fdata['dt']
-    tmps3cost = s3fdata['cost']
-    for i in range(len(tmps3cost)):
-        dt = s3labels[i]
-        idx = np.where(labels==dt)
-        if len(idx) > 1:
-            idx = idx[0]
-        s3cost[idx] = tmps3cost[i]
+    # select only the ukmon data
+    fltr=np.logical_or(costdata['tag']==' billingtag$ukmon',costdata['tag']==' billingtag$ukmonstuff')
+    fltrdata = costdata[fltr]
 
+    # list of services in use
+    svcs = list(set(fltrdata['service']))
+    svcs.sort()
+
+    # get dates
+    s3f=fltrdata['service']==' Amazon Simple Storage Service'
+    s3data = fltrdata[s3f]
+    labels = s3data['dt']
+
+    # create plot
+    fig, ax = plt.subplots()
     width = 0.35       # the width of the bars: can also be len(x) sequence
     labs = []
     for lab in labels:
         labs.append(lab[8:10])
 
-    # print(mth, labs)
-    fig, ax = plt.subplots()
-    maxy = np.ceil(max(s3cost)+max(ec2cost))
-    totcost = np.round(sum(s3cost) + sum(ec2cost),2)
-
-    ax.bar(labs, s3cost, width, label='S3')
-    ax.bar(labs, ec2cost, width, bottom=s3cost, label='EC2')
+    numdays = len(labels)
+    # run through services adding them to the graph
+    bottoms=np.zeros(numdays)
+    for svc in svcs: 
+        thisfltr = fltrdata['service']==svc
+        thisdata = fltrdata[thisfltr]
+        vals=np.zeros(numdays)
+        for dta in thisdata:
+            dt = dta['dt']
+            val = dta['cost']
+            idx = list(labels).index(dt)
+            vals[idx] += val
+        if sum(vals) > 0.1: 
+            svcname = getSvcName(svc)
+            #print(svcname, vals)
+            ax.bar(labs, vals, width, label=svcname, bottom=bottoms)
+            bottoms += vals
+    
+    #maxy = np.ceil(max(s3cost)+max(ec2cost))
+    totcost = np.round(sum(fltrdata['cost']),2)
 
     ax.set_ylabel('Cost ($)')
     ax.set_xlabel('Day of Month')
-    ax.set_title('Cost for month starting {}: total ${:.2f}'.format(mthdate, totcost))
+    ax.set_title(f'Cost for month starting {mthdate}: total ${totcost:.2f}')
     ax.legend()
-    plt.ylim(0,maxy)
 
-    #plt.show()
     os.makedirs(os.path.join(outdir, 'plots'), exist_ok=True)
     fname = os.path.join(outdir, 'plots','{}-{}'.format(accountid, mthdate))
     plt.savefig(fname)
+    plt.show()
 
 
-def getBillingData(ceclient, service, abbrv, dtwanted, endwanted, fldr):
+def getAllBillingData(ceclient, dtwanted, endwanted, regionid, outdir):
+    fldr = os.path.join(outdir, f'{accountid}/{regionid}')
+    os.makedirs(fldr, exist_ok=True)
+
     startdt = dtwanted.strftime('%Y-%m-%d')
     if endwanted is None:
         endwanted = datetime.date.today()
     enddt = endwanted.strftime('%Y-%m-%d')
-    print(startdt, enddt)
+
+    resp=ceclient.get_dimension_values(
+        TimePeriod={'Start':startdt, 'End':enddt}, 
+        Dimension='SERVICE')
+    if resp['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print('unable to retrieve service list')
+        return 
+
+    dims = resp['DimensionValues']
+    svcs=[]
+    for dim in dims:
+        svcs.append(dim['Value'])
 
     response = ceclient.get_cost_and_usage(
         TimePeriod={'Start': startdt, 'End': enddt},
         Granularity='DAILY',
-        Filter={'Dimensions': {'Key': 'SERVICE', 'Values': [service]}},
-        Metrics=['BlendedCost', 'UsageQuantity'],
-        GroupBy=[{'Type': 'DIMENSION','Key': 'SERVICE'}, {'Type': 'TAG','Key': 'billingtag'}])
+        Filter={'Dimensions': {'Key': 'SERVICE', 'Values': svcs}},
+        Metrics=['BlendedCost'], # 'UsageQuantity'],
+        GroupBy=[{'Type': 'DIMENSION','Key': 'SERVICE'}, 
+            {'Type': 'TAG','Key': 'billingtag'}])
 
-    fname = os.path.join(fldr, '{}-{}.json'.format(abbrv, startdt))
-    strdata = json.dumps(response, indent=4)
-    with open(fname, "w") as write_file:
-        write_file.write(strdata)
-    interpretCostData(response, startdt, abbrv, fldr)
-    return 
-
-
-def interpretCostData(srcdata, mthdate, abbrv, fldr):
-    fname = os.path.join(fldr, '{}-{}.csv'.format(abbrv, mthdate))
-    with open(fname, 'w') as csvf:
-        for results in srcdata['ResultsByTime']:
-            daterange = results['TimePeriod']['Start'] 
-            costdata = results['Groups']
-            for dta in costdata:
-                tag = dta['Keys'][1]
-                cost = dta['Metrics']['BlendedCost']['Amount']
-                amt = dta['Metrics']['UsageQuantity']['Amount']
-                csvf.write('{},{},{},{}\n'.format(daterange, tag, cost, amt))
-    return
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print('unable to retrieve costs by service')
+        return 
+    else:
+        results = response['ResultsByTime']
+        costsfile = os.path.join(fldr, f'costs-{startdt[:7]}.csv')
+        with open(costsfile,'w') as outf:
+            outf.write('Date,Service,Tag,Amount\n')
+            for res in results:
+                strt = res['TimePeriod']['Start']
+                for grp in res['Groups']:
+                    svc = grp['Keys'][0]
+                    tag = grp['Keys'][1]
+                    amt = grp['Metrics']['BlendedCost']['Amount']
+                    outf.write(f'{strt}, {svc}, {tag}, {amt}\n')
 
 
 if __name__ == '__main__':
@@ -121,20 +161,9 @@ if __name__ == '__main__':
 
     stscli = boto3.client("sts")
     accountid = stscli.get_caller_identity()["Account"]
-    fldr = os.path.join(outdir, '{}'.format(accountid), regionid)
-
-    os.makedirs(fldr, exist_ok=True)
-
     myconfig = Config(region_name=regionid)
     ceclient = boto3.client('ce', config=myconfig)
 
-    service = 'Amazon Elastic Compute Cloud - Compute'
-    getBillingData(ceclient, service, 'ec2', dtwanted, endwanted, fldr)
+    getAllBillingData(ceclient, dtwanted, endwanted, regionid, outdir)
 
-    service = 'Amazon Simple Storage Service'
-    getBillingData(ceclient, service, 's3', dtwanted, endwanted, fldr)
-
-    service = 'AWS Lambda'
-    getBillingData(ceclient, service, 'lambda', dtwanted, endwanted, fldr)
-
-    drawBarChart(outdir, accountid, regionid, dtwanted)
+    drawBarChart(outdir, accountid, regionid, dtwanted.strftime('%Y-%m'))
