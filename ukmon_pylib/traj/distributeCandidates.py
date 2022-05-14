3d3e253093a76a6fca7e8733ded47e2b1845bd65
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-05-14 11:38:31 +0100

    Update master DB with distributed results

--- ukmon_pylib/traj/distributeCandidates.py
+++ ukmon_pylib/traj/distributeCandidates.py
@@ -15,6 +15,16 @@ import time
 import pickle
 import tarfile
 
+# imports are required to load the picklefiles
+from wmpl.Trajectory.CorrelateRMS import TrajectoryReduced, DatabaseJSON # noqa: F401
+from wmpl.Trajectory.CorrelateRMS import MeteorObsRMS, PlateparDummy, MeteorPointRMS # noqa: F401
+
+
+class dummyMeteorObsRMS(object):
+    def __init__(self, station_code, id):
+        self.station_code = station_code
+        self.id = id
+
 
 def createTaskTemplate(rundate, buckname, spandays=3):
     d1 = (rundate + datetime.timedelta(days = -(spandays-1))).strftime('%Y%m%d')
@@ -143,6 +153,36 @@ def unpackResults(targdir, buckname):
     with tarfile.open(tarfname, 'r:gz') as tar:
         tar.extractall(path=targdir)
     return 
+
+
+def mergeDatabases(targdir, fldrname, masterfile):
+    newdb = os.path.join(targdir, fldrname + '.json')
+    mergedb = DatabaseJSON(newdb)
+    mastdb = DatabaseJSON(masterfile)
+    mastdb.db_file_path = masterfile
+    # merge successful trajectories
+    for traj in mergedb.trajectories:
+        traj_obj = TrajectoryReduced(traj, json_dict = mergedb.trajectories[traj].__dict__)
+        traj_file_path = traj_obj.traj_file_path
+        traj_file_path = '/home/ec2-user/data/RMSCorrelate/' + traj_file_path[traj_file_path.find('trajectories'):]
+        traj_obj.traj_file_path = traj_file_path
+        mastdb.addTrajectory(traj_file_path, traj_obj=traj_obj)
+    # merge failed trajectories 
+    for traj in mergedb.failed_trajectories:
+        traj_obj = TrajectoryReduced(traj, json_dict = mergedb.failed_trajectories[traj].__dict__)
+        traj_file_path = traj_obj.traj_file_path
+        traj_file_path = '/home/ec2-user/data/RMSCorrelate/' + traj_file_path[traj_file_path.find('trajectories'):]
+        traj_obj.traj_file_path = traj_file_path
+        mastdb.addTrajectory(traj_file_path, traj_obj=traj_obj, failed=True)
+    # merge paired obs data
+    for p in mergedb.paired_obs:
+        ids = mergedb.paired_obs[p]
+        for id in ids:
+            met_obs = dummyMeteorObsRMS(p, id)
+            mastdb.addPairedObservation(met_obs)
+    # save the master DB again
+    mastdb.save()
+    return
 
 
 if __name__ == '__main__':
