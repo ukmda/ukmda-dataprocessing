74049dae71b1f3e1ac90918dde0fddb6a13f2676
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-05-18 20:39:45 +0100

    Working version

--- ukmon_pylib/traj/distributeCandidates.py
+++ ukmon_pylib/traj/distributeCandidates.py
@@ -1,7 +1,7 @@
 #
 # Used when running the correlator in 'distributed' mode. 
-# Pick up new groups of candidate trajectories and distribute them, then
-# wait for completion and unpack the resultss. 
+# Pick up new groups of candidate trajectories and distribute them, 
+# then wait for completion and gather the logs. 
 #
 
 import json
@@ -14,17 +14,6 @@ import datetime
 import boto3
 import time
 import pickle
-import tarfile
-
-# imports are required to load the picklefiles
-from wmpl.Trajectory.CorrelateRMS import TrajectoryReduced, DatabaseJSON # noqa: F401
-from wmpl.Trajectory.CorrelateRMS import MeteorObsRMS, PlateparDummy, MeteorPointRMS # noqa: F401
-
-
-class dummyMeteorObsRMS(object):
-    def __init__(self, station_code, id):
-        self.station_code = station_code
-        self.id = id
 
 
 def getClusterDetails(templdir):
@@ -37,16 +26,19 @@ def getClusterDetails(templdir):
     exrolearn = lis[3].strip()
     loggrp = lis[4].strip()
     contname = lis[5].strip()
-    return clusname, secgrp, subnet, exrolearn, loggrp, contname
+    return [clusname, secgrp, subnet, exrolearn, loggrp, contname]
 
 
-def createTaskTemplate(rundate, buckname, spandays=3):
+def createTaskTemplate(rundate, buckname, clusdets, spandays=3):
     d1 = (rundate + datetime.timedelta(days = -(spandays-1))).strftime('%Y%m%d')
     d2 = (rundate + datetime.timedelta(days = 1)).strftime('%Y%m%d')
 
     templdir,_ = os.path.split(__file__)
     templatefile = os.path.join(templdir, 'taskrunner.json')
-    clusname, secgrp, subnet, exrolearn, _ = getClusterDetails(templdir)
+    clusname = clusdets[0]
+    secgrp = clusdets[1]
+    subnet = clusdets[2]
+    exrolearn = clusdets[3]
 
     _, buckname = os.path.split(buckname)
     with open(templatefile) as inf:
@@ -70,9 +62,16 @@ def getDebugStatus():
         return False
 
 
-def distributeCandidates(rundate, srcdir, targdir, maxcount=20):
+def distributeCandidates(rundate, srcdir, targdir, clusdets, maxcount=20):
+
+    clusname = clusdets[0]
 
     client = boto3.client('ecs', region_name='eu-west-2')
+    status = client.describe_clusters(clusters=[clusname])
+    if len(status['clusters']) == 0:
+        print('cluster not running!')
+        return False
+
     print(f'Reading from {srcdir}')
     # obtain a list of picklefiles and sort by name
     flist = glob.glob1(srcdir, '*.pickle')
@@ -103,7 +102,7 @@ def distributeCandidates(rundate, srcdir, targdir, maxcount=20):
                 shutil.copy2(src, dst)
                 outf.write(f'{fli}\n')
 
-        taskjson = createTaskTemplate(rundate, bucknames[i])
+        taskjson = createTaskTemplate(rundate, bucknames[i], clusdets)
 
         response = client.run_task(**taskjson)
         while len(response['tasks']) == 0:
@@ -116,7 +115,7 @@ def distributeCandidates(rundate, srcdir, targdir, maxcount=20):
         if isDbg is True and i == 3:
             break
     
-    # may not be 20 tasks
+    # in debug mode, we don't fill the arrays entirely
     taskarns = [e for e in taskarns if e is not None]
     bucknames = [e for e in bucknames if e is not None]
     jsontempls = [e for e in jsontempls if e is not None]
@@ -126,27 +125,27 @@ def distributeCandidates(rundate, srcdir, targdir, maxcount=20):
     pickle.dump(dmpdata, open(buckroot + '.pickle','wb'))
 
     status = client.describe_clusters(clusters=[clusname])
-    if len(status['clusters']) ==0:
-        print('cluster not running!')
-    else:
-        rtc = status['clusters'][0]['runningTasksCount']
-        ptc = status['clusters'][0]['pendingTasksCount']
-        print(f'{rtc} running, {ptc} pending')
+    rtc = status['clusters'][0]['runningTasksCount']
+    ptc = status['clusters'][0]['pendingTasksCount']
+    print(f'{rtc} running, {ptc} pending')
 
-    return
+    return True
 
 
-def monitorProgress(rundate, targdir):
+def monitorProgress(rundate, targdir, clusdets):
     client = boto3.client('ecs', region_name='eu-west-2')
 
+    # load the buckets, tasks and cluster name from the dump file
     picklefile = os.path.join(targdir, rundate.strftime('%Y%m%d') + '.pickle')
     dumpdata = pickle.load(open(picklefile,'rb'))
     bucknames = dumpdata[0]
     taskarns = dumpdata[1]
     clusname = dumpdata[2]
 
-    templdir,_ = os.path.split(__file__)
-    _, _, _, _, loggrp, contname = getClusterDetails(templdir)
+    # details of the loggroup and container name 
+    loggrp = clusdets[4]
+    contname = clusdets[5]
+
     # need to keep list of task ARNs and then check if they started properly.
     # Look for "lastStatus": "STOPPED",
     # if not "stopCode": "EssentialContainerExited"
@@ -172,6 +171,8 @@ def monitorProgress(rundate, targdir):
                     bucknames.pop(idx)
                     taskcount -= 1
                     print('task completed')
+
+                    # collect the logs from CloudWatch 
                     os.makedirs(os.path.join(targdir, 'logs'), exist_ok=True)
                     with open(os.path.join(targdir, 'logs', f'{thisarn[51:]}.log'), 'w') as outf:
                         for events in getLogDetails(loggrp, thisarn[51:], contname):
@@ -208,47 +209,6 @@ def getLogDetails(loggrp, thisarn, contname, region_name='eu-west-2'):
         yield response['events']
 
 
-def unpackResults(targdir, buckname, remove=False):
-    outdir = os.path.join(targdir, 'solved')
-    os.makedirs(outdir, exist_ok=True)
-    with tarfile.open(buckname + '.tgz', 'r:gz') as tar:
-        tar.extractall(path=outdir)
-    if remove is True:
-        os.remove(buckname + '.tgz')
-        shutil.rmtree(buckname)
-    return 
-
-
-def mergeDatabases(targdir, fldrname, masterfile):
-    newdb = os.path.join(targdir, fldrname + '.json')
-    mergedb = DatabaseJSON(newdb)
-    mastdb = DatabaseJSON(masterfile)
-    mastdb.db_file_path = masterfile
-    # merge successful trajectories
-    for traj in mergedb.trajectories:
-        traj_obj = TrajectoryReduced(traj, json_dict = mergedb.trajectories[traj].__dict__)
-        traj_file_path = traj_obj.traj_file_path
-        traj_file_path = '/home/ec2-user/data/RMSCorrelate/' + traj_file_path[traj_file_path.find('trajectories'):]
-        traj_obj.traj_file_path = traj_file_path
-        mastdb.addTrajectory(traj_file_path, traj_obj=traj_obj)
-    # merge failed trajectories 
-    for traj in mergedb.failed_trajectories:
-        traj_obj = TrajectoryReduced(traj, json_dict = mergedb.failed_trajectories[traj].__dict__)
-        traj_file_path = traj_obj.traj_file_path
-        traj_file_path = '/home/ec2-user/data/RMSCorrelate/' + traj_file_path[traj_file_path.find('trajectories'):]
-        traj_obj.traj_file_path = traj_file_path
-        mastdb.addTrajectory(traj_file_path, traj_obj=traj_obj, failed=True)
-    # merge paired obs data
-    for p in mergedb.paired_obs:
-        ids = mergedb.paired_obs[p]
-        for id in ids:
-            met_obs = dummyMeteorObsRMS(p, id)
-            mastdb.addPairedObservation(met_obs)
-    # save the master DB again
-    mastdb.save()
-    return
-
-
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         rundt = datetime.datetime(2022,4,21)
@@ -259,6 +219,8 @@ if __name__ == '__main__':
     srcdir = os.path.join(matchdir, 'distrib', 'candidates')
     targdir = os.path.join(matchdir, 'distrib')
 
-    distributeCandidates(rundt, srcdir, targdir)
+    templdir,_ = os.path.split(__file__)
+    clusdets = getClusterDetails(templdir)
 
-    monitorProgress(rundt, targdir)
+    distributeCandidates(rundt, srcdir, targdir, clusdets)
+    monitorProgress(rundt, targdir, clusdets)
