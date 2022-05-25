1160fdb5d44604223affff8a3c0df27460ed1da5
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-05-25 22:32:00 +0100

    correct logging message

--- archive/analysis/runDistrib.sh
+++ archive/analysis/runDistrib.sh
@@ -36,18 +36,18 @@ rundate=$(date --date="-$MATCHEND days" '+%Y%m%d')
 source $SERVERAWSKEYS
 AWS_DEFAULT_REGION=eu-west-2 
 
-logger -s -t runMatching "checking correlation server status and starting it"
+logger -s -t runDistrib "checking correlation server status and starting it"
 stat=$(aws ec2 describe-instances --instance-ids $SERVERINSTANCEID --query Reservations[*].Instances[*].State.Code --output text)
 if [ $stat -eq 80 ]; then 
     aws ec2 start-instances --instance-ids $SERVERINSTANCEID
 fi
-logger -s -t runMatching "waiting for the server to be ready"
+logger -s -t runDistrib "waiting for the server to be ready"
 while [ "$stat" -ne 16 ]; do
     sleep 5
     echo "checking"
     stat=$(aws ec2 describe-instances --instance-ids $SERVERINSTANCEID --query Reservations[*].Instances[*].State.Code --output text)
 done
-logger -s -t runMatching "ready to use"
+logger -s -t runDistrib "ready to use"
 
 logger -s -t runDistrib "running phase 1 for dates ${begdate} to ${rundate}"
 source ~/venvs/${WMPL_ENV}/bin/activate
@@ -57,7 +57,7 @@ execMatchingsh=/tmp/execdistrib.sh
 python -m traj.createDistribMatchingSh $MATCHSTART $MATCHEND $execMatchingsh
 chmod +x $execMatchingsh
 
-logger -s -t runMatching "get server details"
+logger -s -t runDistrib "get server details"
 privip=$(aws ec2 describe-instances --instance-ids $SERVERINSTANCEID --query Reservations[*].Instances[*].PrivateIpAddress --output text)
 while [ "$privip" == "" ] ; do
     sleep 5
@@ -65,7 +65,7 @@ while [ "$privip" == "" ] ; do
     privip=$(aws ec2 describe-instances --instance-ids $SERVERINSTANCEID --query Reservations[*].Instances[*].PrivateIpAddress --output text)
 done
 
-logger -s -t runMatching "deploy the script to the server and run it"
+logger -s -t runDistrib "deploy the script to the server and run it"
 
 scp -i $SERVERSSHKEY $execMatchingsh ec2-user@$privip:/tmp
 while [ $? -ne 0 ] ; do
@@ -76,7 +76,10 @@ while [ $? -ne 0 ] ; do
 done 
 ssh -i $SERVERSSHKEY ec2-user@$privip $execMatchingsh
 
-logger -s -t runMatching "monitoring and waiting for completion"
+logger -s -t runDistrib "job run, stop the server again"
+aws ec2 stop-instances --instance-ids $SERVERINSTANCEID
+
+logger -s -t runDistrib "monitoring and waiting for completion"
 source $SERVERAWSKEYS
 targdir=$MATCHDIR/distrib
 python -c "from traj.distributeCandidates import monitorProgress as mp; mp('${rundate}','${targdir}'); "
@@ -98,5 +101,6 @@ mv $targdir/${rundate}.pickle $DATADIR/distrib
 tar czvf ${rundate}.tgz ${rundate}*.json ${rundate}.pickle
 cp ${rundate}.tgz $targdir/done
 rm -f $targdir/${rundate}*.json ${rundate}*.json ${rundate}.pickle
+
 logger -s -t runDistrib "done"
 
