047654b2b9bf9da85ea55e1c1ab3b2b3949723e2
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-05-27 22:39:51 +0100

    patch trajdb location in the database

--- ukmon_pylib/traj/consolidateDistTraj.py
+++ ukmon_pylib/traj/consolidateDistTraj.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import shutil
 
 from wmpl.Trajectory.CorrelateRMS import TrajectoryReduced, DatabaseJSON 
 #from wmpl.Trajectory.CorrelateRMS import MeteorObsRMS, PlateparDummy, MeteorPointRMS # noqa: F401
@@ -48,9 +49,10 @@ def mergeDatabases(srcdir, srcdb, masterpth, masterfile):
 def patchTrajDB(dbfile, targpath, oldstr='/home/ec2-user/data/RMSCorrelate'):
 
     with open(dbfile, 'r') as inf:
-        with open(os.path.join(targpath, 'processed_trajectories.json'), 'w') as outf:
+        with open(os.path.join('/tmp/processed_trajectories.json'), 'w') as outf:
             for lin in inf:
-                outf.write(lin.replace(oldstr, targpath))            
+                outf.write(lin.replace(oldstr, targpath))
+    shutil.copyfile('/tmp/processed_trajectories.json', dbfile)
     return 
 
 
@@ -108,3 +110,4 @@ if __name__ == '__main__':
         #countDataInDb(os.path.join(srcdir, fl))
         print(fl)
         mergeDatabases(srcdir, fl, masterpth, masterdb)
+    patchTrajDB(masterdb, masterpth, oldstr='/home/ec2-user/prod/data/distrib')
