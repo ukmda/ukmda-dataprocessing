218faf237f2189f8a12a6487923c04eff362bce6
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-06 20:23:32 +0100

    bugfix in toParquet

--- ukmon_pylib/converters/toParquet.py
+++ ukmon_pylib/converters/toParquet.py
@@ -8,7 +8,12 @@ df = pd.read_csv(sys.argv[1], skipinitialspace=True)
 
 fn, _ = os.path.splitext(sys.argv[1])
 
+# rename columns to be unique
 df = df.rename(columns={'m':'mi'})
 df = df.rename(columns={'_m_ut':'_mi_ut'})
+
+if 'match' in fn:
+    # fill in any #NAs in the mjd column
+    df.mjd.fillna(df._mjd, inplace=True)
 
 df.to_parquet(fn + '.parquet.gzip', compression='gzip')
