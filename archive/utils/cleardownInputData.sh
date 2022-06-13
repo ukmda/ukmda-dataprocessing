5e505b655451078f9ff1c5787b35086afbc57744
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-13 17:13:53 +0100

    further work to cleardown the raw data

--- archive/utils/cleardownInputData.sh
+++ archive/utils/cleardownInputData.sh
@@ -7,15 +7,14 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/../config/config.ini >/dev/null 2>&1
 source $HOME/venvs/$WMPL_ENV/bin/activate
 
-if [ $# -lt 1 ] ; then
-    echo "MUST supply a timeperiod to run for in YYYYMM format"
-    exit 1
-else
-    ym=$1
-fi
+currmth=$(date +%Y%m)
+if [ $# -lt 1 ] ; then echo "Please supply a month to run for in YYYYMM format"; exit 1 ; fi
+ym=$1
+if [ $((currmth-ym)) -lt 2 ] ; then echo "cant clear down current or last month" ; exit 1 ; fi 
 
 echo "Getting list of files for ${ym}"
 find $MATCHDIR/RMSCorrelate -maxdepth 2 -name "UK????_${ym}*"  -type d -exec ls -1d {} \; > /tmp/cleardown${ym}.txt
+
 
 if [ -f $DATADIR/olddata/rawdata-${ym}.tar.gz ] ; then 
     echo "Adding to compressed archive rawdata-${ym}.tar.gz"
