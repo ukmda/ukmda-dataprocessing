90aa794183f8c00618e99bb1aed008a7720f3581
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-14 14:18:18 +0100

    testin codedeploy

--- archive/codeDeploy/setPermissions.sh
+++ archive/codeDeploy/setPermissions.sh
@@ -6,3 +6,4 @@ echo chowning child folders
 chown -R ec2-user:ec2-user /home/ec2-user/test/*
 find /home/ec2-user/test -name "*.py" -exec chmod 0644 {} \;
 find /home/ec2-user/test -name "*.sh" -exec chmod 0754 {} \;
+echo done
