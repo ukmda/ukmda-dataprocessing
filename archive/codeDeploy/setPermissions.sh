93eafd649a6f42be42da29b5a32ff02814036d60
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-14 14:03:03 +0100

    updating permissions for codebuild

--- archive/codeDeploy/setPermissions.sh
+++ archive/codeDeploy/setPermissions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo chowning parent folder
 chown ec2-user:ec2-user /home/ec2-user/test
 chmod  0754 /home/ec2-user/test
+echo chowning child folders
 chown -R ec2-user:ec2-user /home/ec2-user/test/*
 chmod -R 0644 /home/ec2-user/test/*.py
 chmod -R 0754 /home/ec2-user/test/*.sh
