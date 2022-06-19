470aa1a52d6468ebb377618c2a3cbac1de9b7a48
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-19 16:43:52 +0100

    sort out stop/start script

--- archive/utils/stopstart-calcengine.sh
+++ archive/utils/stopstart-calcengine.sh
@@ -7,9 +7,3 @@ source $here/../config/config.ini >/dev/null 2>&1
 source /home/ec2-user/venvs/${RMS_ENV}/bin/activate
 
 aws ec2 $1-instances --instance-ids $SERVERINSTANCEID --region eu-west-2
-if [ "$1" == "start " ] ; then 
-    privip=$(aws ec2 describe-instances --instance-ids $SERVERINSTANCEID --query Reservations[*].Instances[*].PrivateIpAddress --output text --region eu-west-2)
-    export BIGSERVER=$privip
-else
-    export BIGSERVER=
-fi 
