59a67020265f0c8dd83ecb1105ebceaa4d8f721e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-07-01 22:57:53 +0100

    Additional installs required on the ARM64 graviton build

--- archive/setup/install-geos.sh
+++ archive/setup/install-geos.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-conda activate wmpl
-
-export PROJ4_VERSION=4.9.3
-
-# Compilation work for proj4
-mkdir -p "/tmp/proj-${PROJ4_VERSION}-build"
-pushd "/tmp/proj-${PROJ4_VERSION}-build"
-curl -o "proj-${PROJ4_VERSION}.tar.gz" \
-    "http://download.osgeo.org/proj/proj-${PROJ4_VERSION}.tar.gz" \
-    && tar xfz "proj-${PROJ4_VERSION}.tar.gz"
-rm -f proj-${PROJ4_VERSION}.tar.gz
-
-cd "/tmp/proj-${PROJ4_VERSION}-build/proj-${PROJ4_VERSION}"
-./configure --prefix=/usr/local/proj4
-
-# Make in parallel with 2x the number of processors.
-make -j $(( 2 * $(cat /proc/cpuinfo | egrep ^processor | wc -l) )) \
- && sudo make install \
- && sudo ldconfig
-
-popd
-
 export GEOS_VERSION=3.7.2
 
 # Compilation work for geos
@@ -42,3 +20,4 @@ make -j $(( 2 * $(cat /proc/cpuinfo | egrep ^processor | wc -l) )) \
  && sudo ldconfig
 
 popd
+
