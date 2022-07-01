bc454a68c777a9b1391d96a9ad0f897775eac978
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-07-01 22:57:53 +0100

    Additional installs required on the ARM64 graviton build

--- archive/setup/install-cartopy.sh
+++ archive/setup/install-cartopy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CARTOPY_VERSION=0.19.0
+export CARTOPY_VERSION=0.20.3
 
 mkdir -p "/tmp/cartopy-${CARTOPY_VERSION}-build"
 pushd "/tmp/cartopy-${CARTOPY_VERSION}-build"
@@ -9,8 +9,9 @@ cd cartopy
 git fetch
 git switch --detach tags/v${CARTOPY_VERSION}
 export CPATH=/usr/local/proj4/include
-export PATH=$PATH:/usr/local/proj4/bin:/usr/local/geos/bin
-export LIBRARY_PATH=/usr/local/proj4/lib:/usr/local/geos/lib
+export PATH=usr/local/proj4/bin:/usr/local/geos/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/proj4/lib:/usr/local/geos/lib
+python setup.py build_ext -I /usr/local/proj4/include -I /usr/local/geos/include 
 python setup.py install
 
 
