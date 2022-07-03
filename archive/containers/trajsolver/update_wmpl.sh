b0a50444e660171ce84137eb4b96054e0440ad85
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-07-03 21:30:10 +0100

    missing dependency exclude files that depend on cartopy or QT

--- archive/containers/trajsolver/update_wmpl.sh
+++ archive/containers/trajsolver/update_wmpl.sh
@@ -3,3 +3,7 @@ cd /mnt/e/dev/meteorhunting/ukmon-shared/archive/containers/trajsolver/
 rsync -avz ukmonhelper:src/wmpldev/* ./WesternMeteorPyLib/ --exclude "build/" --exclude "*.egg*" --exclude "dist/" --exclude ".git/" --exclude "__pycache__/"
 rm -Rf ./WesternMeteorPyLib/wmpl/MetSim
 rm -Rf ./WesternMeteorPyLib/wmpl/CAMO
+cd ./WesternMeteorPyLib/wmpl/Utils/
+mv ./DynamicMassFit.py  ./DynamicMassFit.notused
+# temporary hack till i get cartopy working
+mv ./PlotMap_OSM.py ./PlotMap_OSM.py.notused
