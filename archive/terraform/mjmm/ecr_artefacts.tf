73895e5c9982b98efae2c6aa27d5d694fe88847e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-07-03 14:18:00 +0100

    add testing repo

--- archive/terraform/mjmm/ecr_artefacts.tf
+++ archive/terraform/mjmm/ecr_artefacts.tf
@@ -25,6 +25,23 @@ resource "aws_ecr_repository" "trajsolverrepo" {
     "billingtag" = "ukmon"
   }
 }
+# create an ECR repository for the trajsolver test images
+resource "aws_ecr_repository" "trajsolvertestrepo" {
+  name                 = "ukmon/trajsolvertest"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = aws_kms_key.container_key.arn
+  }
+  tags = {
+    "billingtag" = "ukmon"
+  }
+}
+
 # create an ECR repository for the extrafiles image
 resource "aws_ecr_repository" "extrafilesrepo" {
   name                 = "mjmm-repo1"
@@ -41,6 +58,27 @@ resource "aws_ecr_repository" "extrafilesrepo" {
 
 resource "aws_ecr_lifecycle_policy" "policy1" {
   repository = aws_ecr_repository.trajsolverrepo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+                        "description": "Keep only latest two versions images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 2
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+resource "aws_ecr_lifecycle_policy" "trajssolvertestpolicy" {
+  repository = aws_ecr_repository.trajsolvertestrepo.name
   policy     = <<EOF
 {
     "rules": [
@@ -86,6 +124,9 @@ EOF
 
 output "trajsolverid" {
   value = aws_ecr_repository.trajsolverrepo.repository_url
+}
+output "trajsolvertestid" {
+  value = aws_ecr_repository.trajsolvertestrepo.repository_url
 }
 output "extrafilesid" {
   value = aws_ecr_repository.extrafilesrepo.repository_url
