dc0cec852bf05b20675d361a3d545a01d6543430
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-07-05 23:10:09 +0100

    more work on codedeploy

---
+++ archive/codeDeploy/deploy-archive.ps1
@@ -0,0 +1,24 @@
+# trigger code-deploy to run deployment into EC2
+
+if ($args.count -lt 1 ){
+    write-output "usage: deploy.ps1 prod|dev"
+    exit 1
+}
+if (($args[0].tolower() -ne "dev") -and ($args[0].tolower() -ne "prod")) {
+    write-output "usage: deploy.ps1 prod|dev"
+    exit 1
+}
+$targ = $args[0]
+$now = (get-date -uformat "%FT%H:%M:%S")
+copy-item $PSScriptRoot\appspec-${targ}.yml $PSScriptRoot\..\..\appspec.yml 
+git add $PSScriptRoot\..\..\appspec.yml 
+git commit -m "${targ} build of ukmon-archive on ${now}"
+git push
+
+$appname = "ukmon-archive-ec2"
+$depgroup =  "ukmon-archive" 
+$repo= "markmac99/ukmon-shared"
+
+$commitid = $(git rev-parse HEAD)
+
+echo "aws deploy create-deployment --application-name $appname --deployment-group $depgroup --github-location repository=$repo,commitId=$commitid"
