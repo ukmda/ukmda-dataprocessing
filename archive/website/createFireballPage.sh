78ae7472dfbc5af510847395af9ef888a5c58acd
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-07-13 22:20:22 +0100

    bugfix: missing activation of wmpl env

--- archive/website/createFireballPage.sh
+++ archive/website/createFireballPage.sh
@@ -28,6 +28,7 @@ logger -s -t createFireballPage "creating fireball page for $yr"
 mkdir -p $DATADIR/reports/$yr/fireballs > /dev/null 2>&1
 cd $DATADIR/reports/$yr/fireballs
 
+source $HOME/venvs/$WMPL_ENV/bin/activate
 python -m reports.findFireballs ${yr} ALL $2
 
 echo "\$(function() {" > reportindex.js
