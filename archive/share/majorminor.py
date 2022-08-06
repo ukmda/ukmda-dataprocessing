d1651eeaff3626710b309ce9929db9e133e5ddc9
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-08-06 15:52:27 +0100

    move major and minor list to config data

---
+++ archive/share/majorminor.py
@@ -0,0 +1,3 @@
+# importable file that defines the major and minor showers
+majorlist = ['QUA', 'LYR', 'ETA', 'CAP', 'SDA', 'PER', 'ORI', 'NTA', 'STA', 'LEO', 'GEM', 'URS']
+minorlist = ['AUR','SPE','OCT','DRA','EGE','MON']
