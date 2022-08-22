864159f909e8c70e2cc3e72669ffdcaa34b1fea6
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-08-22 18:06:31 +0100

    create camloc db and push to S3

--- archive/cronjobs/nightlyJob.sh
+++ archive/cronjobs/nightlyJob.sh
@@ -18,6 +18,7 @@ logger -s -t nightlyJob "update search index files with singleton data"
 $SRC/analysis/createSearchable.sh
 
 python -c "from fileformats.CameraDetails import updateCamLocDirFovDB; updateCamLocDirFovDB();"
+aws s3 cp $DATADIR/admin/cameraLocs.json s3://ukmon-shared/admin/ --region eu-west-2
 
 # run this only once as it scoops up all unprocessed data
 logger -s -t nightlyJob "looking for matching events and solving their trajectories"
