67b8fe7be3859e9bc1daedc2ef2697396ebd6fd3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-09-15 20:24:03 +0100

    bugfix: don't reprocess if orbit not found

--- archive/utils/deleteOrbit.sh
+++ archive/utils/deleteOrbit.sh
@@ -7,13 +7,15 @@ traj=$1
 ymd=${traj:0:8}
 
 python -c "from traj.manageTraj import deleteDuplicate as dd; dd('$traj'); "
-$SRC/website/createOrbitIndex.sh $ymd
+if [ $? == 1 ] ; then 
+    $SRC/website/createOrbitIndex.sh $ymd
 
-# update the daily report files
-repfiles=$(grep $traj $DATADIR/dailyreports/*.txt| awk -F: '{ print $1 }')
-for repfile in $repfiles ; do
-    cp $repfile $repfile.$$ 
-    cat $repfile  | grep -v $traj > /tmp/newrep.txt
-    mv -f /tmp/newrep.txt $repfile
-done 
-$SRC/website/publishDailyReport.sh
+    # update the daily report files
+    repfiles=$(grep $traj $DATADIR/dailyreports/*.txt| awk -F: '{ print $1 }')
+    for repfile in $repfiles ; do
+        cp $repfile $repfile.$$ 
+        cat $repfile  | grep -v $traj > /tmp/newrep.txt
+        mv -f /tmp/newrep.txt $repfile
+    done 
+    $SRC/website/publishDailyReport.sh
+fi
