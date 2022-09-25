4a60a85973f0ebc6d807465047535b7ecb9595e6
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-09-25 16:20:27 +0100

    improve the container build scripts

--- archive/containers/trajsolver/build.ps1
+++ archive/containers/trajsolver/build.ps1
@@ -9,14 +9,23 @@ $region = "eu-west-2"
 $registry = "${accid}.dkr.ecr.${region}.amazonaws.com"
 $repo = "ukmon/${imagename}"
 
+$yn=read-host -prompt "update WMPL?"
+if ($yn.tolower() -eq "y") { bash -c "./update_wmpl.sh" } 
+
 docker build . -t ${imagename}
 if (! $?)
 {
     write-output "build failed"
     exit
 }
-aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$registry"
-docker tag ${imagename}:latest ${registry}/${repo}:latest
-docker push ${registry}/${repo}:latest
-
+$yn=read-host -prompt "upload to ECR?"
+if ($yn.tolower() -eq "y") {
+    aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$registry"
+    docker tag ${imagename}:latest ${registry}/${repo}:latest
+    docker push ${registry}/${repo}:latest
+}else {
+    write-output "To test the container do docker run -t $imagename 20220924_01"
+    Write-Output "where the last arg is a folder in s3://ukmon-shared/matches/distrib"
+    Write-Output "that contains candidate pickles"
+}
 set-location $loc
--- archive/containers/trajsolver/update_wmpl.sh
+++ archive/containers/trajsolver/update_wmpl.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 cd /mnt/e/dev/meteorhunting/ukmon-shared/archive/containers/trajsolver/
+ssh ukmonhelper "cd src/wmpldev && git stash && git checkout forcontainer && git pull"
 rsync -avz ukmonhelper:src/wmpldev/* ./WesternMeteorPyLib/ --exclude "build/" --exclude "*.egg*" --exclude "dist/" --exclude ".git/" --exclude "__pycache__/"
+ssh ukmonhelper "cd src/wmpldev && git checkout - && git stash apply"
+# remove or rename modules and code that don't work in a container
 rm -Rf ./WesternMeteorPyLib/wmpl/MetSim
 rm -Rf ./WesternMeteorPyLib/wmpl/CAMO
 cd ./WesternMeteorPyLib/wmpl/Utils/
-mv ./DynamicMassFit.py  ./DynamicMassFit.notused
+# references a module that requires the GUI, which isn't present in a container
+mv ./DynamicMassFit.py  ./DynamicMassFit.py.notused
 # temporary hack till i get cartopy working
 mv ./PlotMap_OSM.py ./PlotMap_OSM.py.notused
