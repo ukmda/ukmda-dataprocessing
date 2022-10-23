203c8d2e67ab601d7494e8647849daef2aacde4b
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-10-23 12:03:58 +0100

    improvements in shower report generation

--- archive/website/createMthlyExtracts.sh
+++ archive/website/createMthlyExtracts.sh
@@ -27,22 +27,25 @@ else
 fi 
 
 logger -s -t createMthlyExtracts "gathering annual data"
-cd $DATADIR/browse/annual/
-yr=$(date +%Y)
-rsync -avz ${DATADIR}/matched/matches*.csv .
-rsync -avz ${DATADIR}/consolidated/M*.csv .
-rsync -avz ${DATADIR}/consolidated/P*.csv .
 
 cd $DATADIR/matched
 logger -s -t createMthlyExtracts "creating extracts"
 
 for yr in $yrs
 do
+    rsync -avz ${DATADIR}/matched/matches*${yr}.csv $DATADIR/browse/annual/
+    rsync -avz ${DATADIR}/consolidated/M_${yr}*.csv $DATADIR/browse/annual/
+    rsync -avz ${DATADIR}/consolidated/P_${yr}*.csv $DATADIR/browse/annual/
     python -m reports.extractors $yr $mth $endmths
 done
 
-logger -s -t createMthlyExtracts "done gathering data, creating table"
+logger -s -t createMthlyExtracts "done gathering data, creating monthly table"
 idxfile=$DATADIR/browse/monthly/browselist.js
+
+# sync the raw data to the website so that it has the latest files - 
+# Essential as we're using the content of the website to build the pages
+aws s3 sync $DATADIR/browse/monthly/  $WEBSITEBUCKET/browse/monthly/ --quiet
+aws s3 sync $DATADIR/browse/annual/  $WEBSITEBUCKET/browse/annual/ --quiet
 
 echo "\$(function() {" > $idxfile
 echo "var table = document.createElement(\"table\");" >> $idxfile
@@ -51,46 +54,33 @@ echo "var header = table.createTHead();" >> $idxfile
 echo "header.className = \"h4\";" >> $idxfile
 
 cd $DATADIR/browse/monthly/
+# get a list of files on the website
+aws s3 ls $WEBSITEBUCKET/browse/monthly/ | awk '{ print $4 }' | grep csv > /tmp/browsemth.txt
 yr=$(date +%Y)
 
 while [ $yr -gt 2012 ]
 do
     for mth in {12,11,10,09,08,07,06,05,04,03,02,01}
     do
-        ufobn=""
-        rmsbn=""
-        matbn=""
-        rmsshwr=""
-        if compgen -G "$DATADIR/browse/monthly/${yr}${mth}-detections-ufo.csv" > /dev/null ; then 
-            ufodets=$(ls -1 $DATADIR/browse/monthly/${yr}${mth}-detections-ufo.csv)
-            ufobn=$(basename $ufodets)
-        fi
-        if compgen -G "$DATADIR/browse/monthly/${yr}${mth}-detections-rms.csv" > /dev/null ; then 
-            rmsdets=$(ls -1 $DATADIR/browse/monthly/${yr}${mth}-detections-rms.csv)
-            rmsbn=$(basename $rmsdets)
-        fi
-        if compgen -G "$DATADIR/browse/monthly/${yr}${mth}-rms-shwr.csv" > /dev/null ; then 
-            rmsdets=$(ls -1 $DATADIR/browse/monthly/${yr}${mth}-rms-shwr.csv)
-            rmsshwr=$(basename $rmsdets)
-        fi
-        if compgen -G "$DATADIR/browse/monthly/${yr}${mth}-matches.csv" > /dev/null ; then 
-            matches=$(ls -1 $DATADIR/browse/monthly/${yr}${mth}-matches.csv)
-            matbn=$(basename $matches)
-        fi
-        if [[ "$ufodets" != "" ]] || [[ "$rmsdets" != "" ]] || [[ "$matches" != "" ]] ; then
+        ufobn=$(grep ${yr}${mth}-detections-ufo.csv /tmp/browsemth.txt)
+        rmsbn=$(grep ${yr}${mth}-detections-rms.csv /tmp/browsemth.txt)
+        rmshw=$(grep ${yr}${mth}-rms-shwr.csv /tmp/browsemth.txt)
+        matbn=$(grep ${yr}${mth}-matches.csv /tmp/browsemth.txt)
+        if [[ "$ufobn" != "" ]] || [[ "$rmsbn" != "" ]] || [[ "$matbn" != "" ]] || [[ "$rmsshw" != "" ]] ; then
             echo "var row = table.insertRow(-1);" >> $idxfile
             echo "var cell = row.insertCell(0);" >> $idxfile
             echo "cell.innerHTML = \"<a href="./$ufobn">$ufobn</a>\";" >> $idxfile
             echo "var cell = row.insertCell(1);" >> $idxfile
             echo "cell.innerHTML = \"<a href="./$rmsbn">$rmsbn</a>\";" >> $idxfile
             echo "var cell = row.insertCell(2);" >> $idxfile
-            echo "cell.innerHTML = \"<a href="./$rmsshwr">$rmsshwr</a>\";" >> $idxfile
+            echo "cell.innerHTML = \"<a href="./$rmshw">$rmshw</a>\";" >> $idxfile
             echo "var cell = row.insertCell(3);" >> $idxfile
             echo "cell.innerHTML = \"<a href="./$matbn">$matbn</a>\";" >> $idxfile
         fi
     done
     yr=$((yr-1))
 done
+\rm /tmp/browsemth.txt
 
 echo "var row = header.insertRow(0);" >> $idxfile
 echo "var cell = row.insertCell(0);" >> $idxfile
@@ -113,9 +103,11 @@ echo "})" >> $idxfile
 logger -s -t createMthlyExtracts "js table created, copying to website"
 aws s3 sync $DATADIR/browse/monthly/  $WEBSITEBUCKET/browse/monthly/ --quiet
 
-logger -s -t createMthlyExtracts "done gathering data, creating table"
+logger -s -t createMthlyExtracts "creating annual table"
 idxfile=$DATADIR/browse/annual/browselist.js
 yr=$(date +%Y)
+# get a list of files on the website
+aws s3 ls $WEBSITEBUCKET/browse/annual/ | awk '{ print $4 }' | grep csv > /tmp/browseann.txt
 
 echo "\$(function() {" > $idxfile
 echo "var table = document.createElement(\"table\");" >> $idxfile
@@ -124,22 +116,14 @@ echo "var header = table.createTHead();" >> $idxfile
 echo "header.className = \"h4\";" >> $idxfile
 
 while [ $yr -gt 2012 ] ; do
-    ufobn=""
-    rmsbn=""
-    matbn=""
-    if compgen -G "$DATADIR/browse/annual/M_${yr}-unified.csv" > /dev/null ; then 
-        ufodets=$(ls -1 $DATADIR/browse/annual/M_${yr}-unified.csv)
-        ufobn=$(basename $ufodets)
-    fi
-    if compgen -G "$DATADIR/browse/annual/P_${yr}-unified.csv" > /dev/null ; then 
-        rmsdets=$(ls -1 $DATADIR/browse/annual/P_${yr}-unified.csv)
-        rmsbn=$(basename $rmsdets)
-    fi
-    if compgen -G "$DATADIR/browse/annual/matches-${yr}.csv" > /dev/null ; then 
-        matches=$(ls -1 $DATADIR/browse/annual/matches-${yr}.csv)
-        matbn=$(basename $matches)
-    fi
-    if [[ "$ufodets" != "" ]] || [[ "$rmsdets" != "" ]] || [[ "$matches" != "" ]] ; then
+    ufobn=$(grep M_${yr}-unified.csv /tmp/browseann.txt)
+    rmsbn=$(grep P_${yr}-unified.csv /tmp/browseann.txt)
+    matbn=$(grep matches-full-${yr}.csv /tmp/browseann.txt)
+    if [ "$matbn" == "" ] ; then
+        matbn=$(grep matches-${yr}.csv /tmp/browseann.txt)
+    fi 
+
+    if [[ "$ufobn" != "" ]] || [[ "$rmsbn" != "" ]] || [[ "$matbn" != "" ]] ; then
         echo "var row = table.insertRow(-1);" >> $idxfile
         echo "var cell = row.insertCell(0);" >> $idxfile
         echo "cell.innerHTML = \"<a href="./$ufobn">$ufobn</a>\";" >> $idxfile
@@ -150,6 +134,7 @@ while [ $yr -gt 2012 ] ; do
     fi
     yr=$((yr-1))
 done
+\rm /tmp/browseann.txt
 
 echo "var row = header.insertRow(0);" >> $idxfile
 echo "var cell = row.insertCell(0);" >> $idxfile
