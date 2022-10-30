61a5218b1f6d8c1ce49379c58ad0f1fca9db3cf8
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-10-30 14:55:18 +0000

    rename file to make purpose clearer

--- archive/analysis/reportActiveShowers.sh
+++ archive/analysis/reportActiveShowers.sh
@@ -16,6 +16,8 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/../config.ini >/dev/null 2>&1
 source ~/venvs/$RMS_ENV/bin/activate
+logger -s -t reportActiveShowers "starting"
+$SRC/utils/clearCaches.sh
 
 if [ $# -eq 0 ]; then
     yr=$(date +%Y)
@@ -23,10 +25,11 @@ else
     yr=$1
 fi
 
-logger -s -t reportYear "get list of active showers"
+logger -s -t reportActiveShowers "get list of active showers"
 python -m utils.getActiveShowers | while read i 
 do 
-    logger -s -t reportYear "processing $i for $yr"
+    logger -s -t reportActiveShowers "processing $i for $yr"
     $SRC/analysis/showerReport.sh $i $yr force
 done
-logger -s -t reportYear "Finished"
+$SRC/utils/clearCaches.sh
+logger -s -t reportActiveShowers "finished"
