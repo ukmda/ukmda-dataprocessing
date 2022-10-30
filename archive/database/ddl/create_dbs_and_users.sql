d3200ed7585ed1bd348e0a8a622b5a8e19710979
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-10-30 14:55:55 +0000

    Further work on mariadb database

--- archive/database/ddl/create_dbs_and_users.sql
+++ archive/database/ddl/create_dbs_and_users.sql
@@ -1,9 +1,20 @@
 /* DDL to create users in mariadb */
 
 create database ukmon;
-create or replace user if not exists batch@localhost identified by 'Batch33mdl';
-grant usage on ukmon.* to batch@localhost;
+create user batch@localhost identified by 'Batch33mdl';
+create user batch@'%' identified by 'Batch33mdl';
+grant all privileges on ukmon.* to batch@'%';
+grant all privileges on ukmon.* to batch@localhost;
+
 create database ukmondev;
-create or replace user if not exists batchdev@localhost identified by 'Batchdev33mdl';
-grant usage on ukmondev.* to batchdev@localhost;
+create user batchdev@localhost identified by 'Batchdev33mdl';
+create user batchdev@'%' identified by 'Batchdev33mdl';
+grant all privileges on ukmondev.* to batchdev@'%';
+grant all privileges on ukmondev.* to batchdev@localhost;
+
+grant select on ukmon.* to batchdev@localhost;
+grant select on ukmon.* to batchdev@'%';
+grant select on ukmondev.* to batch@localhost;
+grant select on ukmondev.* to batch@'%';
+
 flush privileges;
