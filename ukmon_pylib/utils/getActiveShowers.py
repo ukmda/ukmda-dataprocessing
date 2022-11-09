983fffd61964348399e0274d44b487e8c63135fa
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-11-09 15:10:05 +0000

    make getActiveShowers callable from other code

--- ukmon_pylib/utils/getActiveShowers.py
+++ ukmon_pylib/utils/getActiveShowers.py
@@ -5,12 +5,20 @@ from fileformats import imoWorkingShowerList as iwsl
 import datetime
 import sys
 
-if len(sys.argv) > 1:
-    targdate = datetime.datetime.strptime(sys.argv[1], '%Y%m%d')
-else:
-    targdate = datetime.datetime.now()
 
-sl = iwsl.IMOshowerList()
-p=sl.getActiveShowers(targdate,True)
-for shwr in p:
-    print(shwr)
+def getActiveShowers(targdate, retlist=False):
+    sl = iwsl.IMOshowerList()
+    listofshowers=sl.getActiveShowers(targdate,True)
+    if retlist is False:
+        for shwr in listofshowers:
+            print(shwr)
+    else:
+        return listofshowers
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        targdate = datetime.datetime.strptime(sys.argv[1], '%Y%m%d')
+    else:
+        targdate = datetime.datetime.now()
+    getActiveShowers(targdate)
