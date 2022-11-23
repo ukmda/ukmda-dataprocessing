b8fff5f05464f2b90f18a130cb7c65c5f48f3f8c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-11-23 15:23:55 +0000

    support to rerun ftptoukmon

--- archive/utils/rerunFTPtoUKMONlambda.sh
+++ archive/utils/rerunFTPtoUKMONlambda.sh
@@ -4,7 +4,7 @@
 
 dt=$1
 if [ "$dt" == "" ] ; then echo "need date" ; exit ; fi 
-if [ -f ./ftp2ukmon.txt ] ; then rm ./ftp2ukmon.txt ; fi
+if [ -f /tmp/ftp2ukmon.txt ] ; then rm /tmp/ftp2ukmon.txt ; fi
 
 # create list of ftpdetect files to reprocess
 ls -1 $SRC/data/single/new/processed/*${dt}*.csv | while read i 
@@ -13,16 +13,14 @@ do
 	sid=${fn:6:6}
 	dtdir=${fn:6:29}
 	ftpn=$(echo $fn | sed 's/ukmon/FTPdetectinfo/g;s/csv/txt/g')
-	echo matches/RMSCorrelate/$sid/$dtdir/$ftpn >> ./ftp2ukmon.txt
+	echo matches/RMSCorrelate/$sid/$dtdir/$ftpn >> /tmp/ftp2ukmon.txt
 done 
 
 # reprocess them 
-cat ftp2ukmon.txt | while read i
+cat /tmp/ftp2ukmon.txt | while read i
 do
 	fullname=${i}
-	cat cftpd_templ.json | sed "s|KEYGOESHERE|${fullname}|g" > tmp.json
+	cat $SRC/utils/cftpd_templ.json | sed "s|KEYGOESHERE|${fullname}|g" > ./tmp.json
 	aws lambda invoke --profile ukmonshared --function-name ftpToUkmon --log-type Tail  --payload file://./tmp.json  --region eu-west-2 --cli-binary-format raw-in-base64-out res.log
 done
-
-
-
+rm ./tmp.json
