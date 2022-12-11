7b68ef60476260c1764aeaa33cef5935f475432d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-12-11 16:12:48 +0000

    Add a few more minor showers

--- archive/share/majorminor.py
+++ archive/share/majorminor.py
@@ -1,3 +1,3 @@
 # importable file that defines the major and minor showers
 majorlist = ['QUA', 'LYR', 'ETA', 'CAP', 'SDA', 'PER', 'AUR', 'ORI', 'NTA', 'STA', 'LEO', 'GEM', 'URS']
-minorlist = ['SPE','OCT','DRA','EGE','MON']
+minorlist = ['SPE','OCT','DRA','EGE','MON','HYD','COM','NOO']
