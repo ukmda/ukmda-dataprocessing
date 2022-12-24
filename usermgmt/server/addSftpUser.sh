c54e1869db73c2c69cdffd7a6d9c16a7f14bee7d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-12-24 12:13:39 +0000

    bugfix i addSftpUser

--- usermgmt/server/addSftpUser.sh
+++ usermgmt/server/addSftpUser.sh
@@ -7,28 +7,29 @@ shortid=$2
 oldloc=$3
 if [ -z $2 ] ; then shortid=$1 ; fi 
 
-cd /home/ec2-user/keymgmt
+keydir=/home/ec2-user/keymgmt
+cd $keydir
 
 logger -s -t addSftpUser "adding user $userid at $shortid $oldloc"
 
 # create the keyfiles in the required format
 counter=0
-ls rawkeys/live/$shortid.key > /dev/null 2>&1
+ls $keydir/rawkeys/live/$shortid.key > /dev/null 2>&1
 while [ $? -ne 0 ] ; do
     sleep 5
-    ls rawkeys/live/$shortid.key > /dev/null 2>&1
+    ls $keydir/rawkeys/live/$shortid.key > /dev/null 2>&1
 done 
-if [ ! -f rawkeys/live/$shortid.key ] ; then 
+if [ ! -f $keydir/rawkeys/live/$shortid.key ] ; then 
     logger -s -t addSftpUser "missing raw key for $shortid"   
     exit 1
 fi 
-python /home/ec2-user/keymgmt/jsonToKeyFile.py rawkeys/live/$shortid.key live
+python $keydir/jsonToKeyFile.py $keydir/rawkeys/live/$shortid.key $keydir/live
 
 # all-lowercase versions of the names
 userid="${userid,,}"
 shortid_l="${shortid,,}"
 
-if [ ! -f live/$shortid_l.key ] ; then 
+if [ ! -f $keydir/live/$shortid_l.key ] ; then 
     logger -s -t addSftpUser "problem creating keyfile"
     exit 1
 fi
@@ -43,31 +44,31 @@ sudo mkdir /var/sftp/$userid/.ssh
 
 # copy the public key to the right place
 logger -s -t addSftpUser "Applying the public key $1"
-dos2unix /home/ec2-user/keymgmt/sshkeys/$1.pub
-cat /home/ec2-user/keymgmt/sshkeys/$1.pub  > /tmp/tmp.pub
+dos2unix $keydir/sshkeys/$1.pub
+cat $keydir/sshkeys/$1.pub  > /tmp/tmp.pub
 sudo cp /tmp/tmp.pub /var/sftp/$userid/.ssh/authorized_keys
 sudo chown -R $userid:$userid /var/sftp/$userid/.ssh/authorized_keys
 rm /tmp/tmp.pub
 
 # add the key to logupload's authorized_keys file
 logger -s -t addSftpUser "Adding the key to loguploads authorized_keys"
-awk 1 ~ec2-user/keymgmt/sshkeys/*.pub > /tmp/logul.pub
+awk 1 $keydir/sshkeys/*.pub > /tmp/logul.pub
 sudo cp /tmp/logul.pub /var/sftp/logupload/.ssh/authorized_keys
 sudo chown logupload:logupload /var/sftp/logupload/.ssh/authorized_keys
 rm /tmp/logul.pub
 
 # copy the files
 logger -s -t addSftpUser "Copying the ini file and aws keyfile"
-cat /home/ec2-user/keymgmt/ukmon.ini | sed "s/STATIONLOCATION/$userid/g" > /home/ec2-user/keymgmt/inifs/$userid.ini
-sudo cp /home/ec2-user/keymgmt/inifs/$userid.ini /var/sftp/$userid/ukmon.ini
-sudo cp /home/ec2-user/keymgmt/live/$shortid_l.key /var/sftp/$userid/live.key
+cat $keydir/ukmon.ini | sed "s/STATIONLOCATION/$userid/g" > /home/ec2-user/keymgmt/inifs/$userid.ini
+sudo cp $keydir/inifs/$userid.ini /var/sftp/$userid/ukmon.ini
+sudo cp $keydir/live/$shortid_l.key /var/sftp/$userid/live.key
 
 # if we are moving a station, update the old ini file 
 # so that the next run points to the new loc
 if [ ! -z $oldloc ] ; then
     logger -s -t addSftpUser "Moving $oldloc to $userid"
     sudo cp /var/sftp/$oldloc/ukmon.ini /var/sftp/$oldloc/ukmon.ini.bkp
-    sudo cp /home/ec2-user/keymgmt/inifs/$userid.ini /var/sftp/$oldloc/ukmon.ini
+    sudo cp $keydir/inifs/$userid.ini /var/sftp/$oldloc/ukmon.ini
 fi 
 sudo ls -ltr /var/sftp/$userid/
 sudo ls -ltr /var/sftp/$userid/.ssh/
