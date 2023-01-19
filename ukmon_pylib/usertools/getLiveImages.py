a24f07f7a0946ebdea23b2eae81483743182e49f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-01-19 23:17:54 +0000

    improvements to getting live data

--- ukmon_pylib/usertools/getLiveImages.py
+++ ukmon_pylib/usertools/getLiveImages.py
@@ -4,21 +4,23 @@
 import os
 import sys
 import boto3
-import tempfile
 from fileformats import ReadUFOCapXML as ufoc
 
 
-def getLiveJpgs(dtstr, outdir):
+def getLiveJpgs(dtstr, outdir=None, create_txt=False, buck_name=None):
+    if outdir is None:
+        outdir = dtstr
     os.makedirs(outdir, exist_ok=True)
+    if buck_name is None:
+        buck_name = os.getenv('UKMONLIVEBUCKET', default='s3://ukmon-live')[5:]
     s3 = boto3.client('s3')
-    buck = os.getenv('UKMONLIVEBUCKET', default='s3://ukmon-live')[5:]
-    x = s3.list_objects_v2(Bucket=buck,Prefix=f'M{dtstr}')
+    x = s3.list_objects_v2(Bucket=buck_name,Prefix=f'M{dtstr}')
     if  x['KeyCount'] > 0:
         print(f"found {x['KeyCount']} records, saving to {outdir}")
         for k in x['Contents']:
             key = k['Key']
             if '.xml' in key:
-                s3.download_file(buck, key, os.path.join(outdir, key))
+                s3.download_file(buck_name, key, os.path.join(outdir, key))
                 x = ufoc.UCXml(os.path.join(outdir, key))
                 fn = x.ucxml['ufocapture_record']['@cap'].strip()
                 os.remove(os.path.join(outdir, key))
@@ -26,7 +28,7 @@ def getLiveJpgs(dtstr, outdir):
                 if len(fn) < 5:
                     outkey = key
                     spls = key.split('_')
-                    stationid = spls[4][:6].lower()
+                    stationid = spls[-1][:6].lower()
                     dtime = key[1:16]
                     patt = f'FF_{stationid}_{dtime}'
                 else:
@@ -34,17 +36,30 @@ def getLiveJpgs(dtstr, outdir):
                     patt = fn[:26]
                     stationid = fn[3:9].lower()
                 print(key)
-                s3.download_file(buck, key, os.path.join(outdir, outkey))
-                txtf = os.path.join(f'{stationid}.txt')
-                if os.path.isfile(txtf):
-                    os.remove(txtf)
-                with open(txtf,'w') as outf:
-                    outf.write(f'{patt}\n{patt.replace("FF_", "FR_")}\n')
-
-
-
+                s3.download_file(buck_name, key, os.path.join(outdir, outkey))
+                if create_txt is True:
+                    createTxtFile(key, outdir)
     else:
         print('no records found')
 
+
+def createTxtFile(fname, outdir):
+    if fname[0] == 'M':
+        spls = fname.split('_')
+        stationid = spls[-1][:6].lower()
+        dtime = fname[1:16]
+        patt = f'FF_{stationid}_{dtime}'
+        stationid = stationid.lower()
+    else:
+        patt = fname[:25]
+        stationid = fname[3:9].lower()
+    txtf = os.path.join(outdir, f'{stationid}.txt')
+    if os.path.isfile(txtf):
+        os.remove(txtf)
+    patt = patt.upper()
+    with open(txtf,'w') as outf:
+        outf.write(f'{patt}\n{patt.replace("FF_", "FR_")}\n')
+    return txtf
+
 if __name__ == '__main__':
-    getLiveJpgs(sys.argv[1], sys.argv[2])
+    getLiveJpgs(sys.argv[1])
