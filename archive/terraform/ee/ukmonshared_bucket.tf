eddaee4e05d5f7fe1dca2b71fd1cf4b770fb8254
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-01-31 22:07:51 +0000

    move logging bucket to its own file

--- archive/terraform/ee/ukmonshared_bucket.tf
+++ archive/terraform/ee/ukmonshared_bucket.tf
@@ -164,63 +164,9 @@ resource "aws_s3_bucket_ownership_controls" "ukmonshare_objownrule" {
   }
 }
 
-resource "aws_s3_bucket" "logbucket" {
-  bucket        = "ukmon-s3-access-logs"
-  force_destroy = false
-  tags = {
-    "billingtag" = "ukmon"
-    "ukmontype"  = "logs"
-  }
-}
-
 resource "aws_s3_bucket_logging" "ukmonsharedlogs" {
   bucket = aws_s3_bucket.ukmonshared.id
 
   target_bucket = aws_s3_bucket.logbucket.id
   target_prefix = "ukmonshared/"
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "ukmonlogslcp" {
-  bucket = aws_s3_bucket.logbucket.id
-  rule {
-    status = "Enabled"
-    id     = "purge old versions"
-    noncurrent_version_expiration {
-      noncurrent_days = 30
-    }
-  }
-  rule {
-    id     = "purge old ukmon-shared logs"
-    status = "Enabled"
-
-    expiration {
-      days                         = 10
-      expired_object_delete_marker = false
-    }
-
-    filter {
-      prefix = "ukmonshared/"
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 5
-    }
-  }
-  rule {
-    id     = "purge old ukmeteornetworkarchive logs"
-    status = "Enabled"
-
-    expiration {
-      days                         = 10
-      expired_object_delete_marker = false
-    }
-
-    filter {
-      prefix = "archsite/"
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 5
-    }
-  }
 }
