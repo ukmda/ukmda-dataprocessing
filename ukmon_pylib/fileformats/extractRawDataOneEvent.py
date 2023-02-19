38adb9faefd403b4ff9aef787ef61d8252e37577
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-02-19 17:22:17 +0000

    filtering data for solver

--- ukmon_pylib/fileformats/extractRawDataOneEvent.py
+++ ukmon_pylib/fileformats/extractRawDataOneEvent.py
@@ -9,14 +9,14 @@ from fileformats.ftpDetectInfo import writeNewFTPFile, loadFTPDetectInfo
 def filterFTPforSpecificTime(ftpfile, dtstr):
     meteor_list = loadFTPDetectInfo(ftpfile, time_offsets=None, join_broken_meteors=True, locdata=None)
     refdt = datetime.datetime.strptime(dtstr, '%Y%m%d_%H%M%S')
-    print(refdt)
+    #print(refdt)
     new_met_list = []
     for met in meteor_list:
-        print(met.ff_name)
+        #print(met.ff_name)
         dtpart = datetime.datetime.strptime(met.ff_name[10:25], '%Y%m%d_%H%M%S')
         tdiff = (refdt - dtpart).seconds
-        print(tdiff)
-        if abs(tdiff) < 11:
+        #print(tdiff)
+        if abs(tdiff) < 21:
             print('adding one entry')
             new_met_list.append(met)
     newname = writeNewFTPFile(ftpfile, new_met_list)
--- ukmon_pylib/tests/makeMaxCamData.py
+++ ukmon_pylib/tests/makeMaxCamData.py
@@ -3,14 +3,10 @@
 import os
 import sys
 import boto3
+import fileformats.extractRawDataOneEvent as erdv
 
 
-camlist = ['UK000S','UK0006','UK0035','UK003C','UK003D','UK0069', 
-           'UK006S','UK0070','UK007A','UK007H','UK007L','UK007Q']
-dtstr = '20230112'
-
-
-def gatherData(camlist, ymd, outdir):
+def gatherData(camlist, ymd, outdir, patt):
     s3bucket='ukmon-shared'
     s3 = boto3.resource('s3')
 
@@ -27,9 +23,22 @@ def gatherData(camlist, ymd, outdir):
                 ppname = os.path.join(outdir, camid, fldr, localf)
                 s3.meta.client.download_file(s3bucket, obj.key, ppname)
             elif '.config' in obj.key:
-                ppname = os.path.join(outdir, camid, fldr, localf)
-                s3.meta.client.download_file(s3bucket, obj.key, ppname)
+                cfgname = os.path.join(outdir, camid, fldr, localf)
+                s3.meta.client.download_file(s3bucket, obj.key, cfgname)
+
+    for camid in camlist:
+        if os.path.isdir(os.path.join(outdir, camid)):
+            dirs = os.listdir(os.path.join(outdir, camid))
+            fils = os.listdir(os.path.join(outdir, camid, dirs[0]))
+            ftpfile = [f for f in fils if 'FTP' in f and 'old' not in f and 'new' not in f]
+            localftpfile = os.path.join(outdir, camid, dirs[0], ftpfile[0])
+            erdv.filterFTPforSpecificTime(localftpfile, patt)
 
 
 if __name__ == '__main__':
-    gatherData(camlist, dtstr, 'MaxCams')
+    camlist = ['UK000S','UK0006','UK0035','UK003C','UK003D','UK0069', 
+            'UK006S','UK0070','UK007A','UK007H','UK007L','UK007Q']
+    dtstr = '20230112'
+    patt = '20230113_021451'
+
+    gatherData(camlist, dtstr, 'MaxCams', patt)
