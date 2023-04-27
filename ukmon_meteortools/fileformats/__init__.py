bb3666ee5be38ab665bc77eb1f4d64c589486ac3
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-04-27 22:06:59 +0100

    Updating documentation

--- ukmon_meteortools/fileformats/__init__.py
+++ ukmon_meteortools/fileformats/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018-2023 Mark McIntyre
 # flake8: noqa
 
-from .ftpDetectInfo import filterFTPforSpecificTime, writeNewFTPFile, loadFTPDetectInfo
+from .ftpDetectInfo import filterFTPforSpecificTime, writeNewFTPFile, loadFTPDetectInfo, MeteorObservation
 from .imoWorkingShowerList import IMOshowerList, majorlist, minorlist
 from .platepar import loadPlatepars, platepar
 from .ReadUFOAnalyzerXML import UAXml
