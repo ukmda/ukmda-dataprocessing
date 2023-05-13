d1cd7fa8d25de8a2d9e27435a31d7727e80067b1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-05-13 21:38:46 +0100

    more tests

---
+++ archive/ukmon_pylib/tests/test_reports_createAnnualBarChart.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2018-2023 Mark McIntyre
+# tests for createExchangeFiles.py
+import os
+
+from reports.createAnnualBarChart import createBarChart
+
+
+here = os.path.split(os.path.abspath(__file__))[0]
+datadir = os.path.join(here, 'data')
+
+
+def test_createBarChart():
+    # pass year only
+    yr=2023
+    res = createBarChart(yr=yr)
+    assert res is not None
+    outf=os.path.join(datadir,f'Annual-{yr}.jpg')
+    assert os.path.isfile(outf)
+    os.remove(outf)
+
+    # pass year and datadir
+    res = createBarChart(datadir=datadir, yr=yr)
+    outf=os.path.join(datadir,f'Annual-{yr}.jpg')
+    assert os.path.isfile(outf)
+    os.remove(outf)
+
+    # pass datadir only
+    res = createBarChart(datadir=datadir, yr=None)
+    outf=os.path.join(datadir,f'Annual-{yr}.jpg')
+    assert os.path.isfile(outf)
+    os.remove(outf)
+
+    # no params passed
+    res = createBarChart()
+    outf=os.path.join(datadir,f'Annual-{yr}.jpg')
+    assert os.path.isfile(outf)
+    os.remove(outf)
+
+    # nonexistent year
+    res = createBarChart(yr=2150)
+    assert res is None
