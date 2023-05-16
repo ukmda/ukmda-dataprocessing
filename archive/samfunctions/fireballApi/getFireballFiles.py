fb9f5679814b94f0cf66b65314354469f503605d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-05-16 21:31:51 +0100

    enhabncement to support platepar creation

--- archive/samfunctions/fireballApi/getFireballFiles.py
+++ archive/samfunctions/fireballApi/getFireballFiles.py
@@ -8,6 +8,24 @@ import datetime
 import json
 
 
+def _getCamLoc(camid):
+    s3 = boto3.client('s3', region_name='eu-west-2')
+    tmpdir = os.getenv('TMP', default='/tmp')
+    localf = os.path.join(tmpdir, 'camera-details.csv')
+    s3.download_file('ukmon-shared', 'consolidated/camera-details.csv', localf)
+    camdets = open(localf, 'r').readlines()
+    os.remove(localf)
+    mtchs = [cam for cam in camdets if camid in cam]
+    if len(mtchs) == 0:
+        return None
+    for li in mtchs:
+        spls = li.split(',')
+        active = int(spls[-1].strip())
+        if active == 1:
+            return spls[0] + '/' + spls[1]
+    return None
+
+
 def getFBfiles(patt):
     """
     Retrieve fireball files from the ukmon website that match a pattern
@@ -53,8 +71,20 @@ def getFBfiles(patt):
             #fname = key.split('/')[-1] # hardcoding it here
             flist.append({'filename': 'platepar_cmn2010.cal', 'url': url})
 
-    # config file, platepars_all and ftpdetect
+    # specimen FITS files where available (useful for calibration)
     dtstr = patt.split('_')[1]
+    camloc = _getCamLoc(camid)
+    fullpatt = f'archive/{camloc}/{dtstr[:4]}/{dtstr[:6]}/{dtstr}/FF_{camid}'
+    x = s3.list_objects_v2(Bucket=buck_name,Prefix=fullpatt)
+    if x['KeyCount'] > 0:
+        for k in x['Contents']:
+            key = k['Key']
+            url = s3.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': buck_name,'Key': key}, ExpiresIn=300)
+            fname = key.split('/')[-1] 
+            flist.append({'filename': fname, 'url': url})
+
+
+    # config file, platepars_all and ftpdetect
     gotcfg = False
     fname = '.config'
     fullpatt = f'matches/RMSCorrelate/{camid}/{camid}_{dtstr}'
