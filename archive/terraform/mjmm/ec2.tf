fe35d1922aec898a3c9b34ff733b2afcf6057234
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-06-27 10:09:32 +0100

    add new t4g server

--- archive/terraform/mjmm/ec2.tf
+++ archive/terraform/mjmm/ec2.tf
@@ -6,54 +6,57 @@ resource "aws_instance" "ukmonhelper" {
   iam_instance_profile = aws_iam_instance_profile.S3FullAccess.name
   key_name             = aws_key_pair.marks_key.key_name
   security_groups      = [aws_security_group.launch-wizard-4.name]
+
+  root_block_device {
+    tags = {
+      "Name"       = "UKMonHelperVol"
+      "billingtag" = "ukmon"
+      "project"    = "UKMonHelper"
+    }
+    volume_size = 50
+    volume_type = "gp3"
+    throughput = 125
+    iops = 3000
+  }
+
   tags = {
     "Name"       = "UKMonHelper"
     "billingtag" = "ukmon"
     "project"    = "UKMonHelper"
   }
 }
-/*
-resource "aws_instance" "CalcEngine4ARM" {
-  #ami                  = "ami-05c6a3a1350a69209"
-  ami                  = "ami-0e15ed7362de6ef5b" # this is my AMI
-  instance_type        = "c6g.4xlarge"
-  iam_instance_profile = aws_iam_instance_profile.S3FullAccess.name
-  key_name             = aws_key_pair.marks_key.key_name
-  security_groups      = [aws_security_group.launch-wizard-4.name]
-  tags = {
-    "Name"       = "Calcengine4Arm"
-    "name"       = "Calcengine4"
-    "billingtag" = "ukmon"
-    "project"    = "UkmonHelperBigx2"
-  }
-  root_block_device {
-    tags = {
-      "Name"       = "calcengine4arm"
-      "billingtag" = "ukmon"
-      "project"    = "UkmonHelperBigx2"
-    }
-    volume_size = 50
-  }
-}
-*/
-/*
-resource "aws_instance" "x16coreAWS" {
-  ami                  = "ami-03ac5a9b225e99b02"
-  instance_type        = "c5.4xlarge"
+
+resource "aws_instance" "ukmonhelper_g" {
+  ami                  = "ami-0bba6371ce54738a5"
+  instance_type        = "t4g.micro"
   iam_instance_profile = aws_iam_instance_profile.S3FullAccess.name
   key_name             = aws_key_pair.marks_key.key_name
   security_groups      = [aws_security_group.ec2publicsg.name]
+
+  root_block_device {
+    tags = {
+      "Name"       = "UKMonHelperVol2"
+      "billingtag" = "ukmon"
+      "project"    = "UKMonHelper2"
+    }
+    volume_size = 50
+    volume_type = "gp3"
+    throughput = 125
+    iops = 3000
+    encrypted = true
+    kms_key_id = aws_kms_key.container_key.id
+  }
+
   tags = {
-    "Name"       = "16coreAWS"
+    "Name"       = "UKMonHelper2"
     "billingtag" = "ukmon"
-    "project"    = "UKMonHelperBig"
+    "project"    = "UKMonHelper2"
   }
 }
-*/
 
 resource "aws_instance" "backuprunner" {
   ami                  = "ami-0d729d2846a86a9e7"
-  instance_type        = "t2.medium"
+  instance_type        = "t3a.medium"
   iam_instance_profile = aws_iam_instance_profile.S3FullAccess.name
   key_name             = aws_key_pair.marks_key.key_name
   security_groups      = [aws_security_group.ec2publicsg.name]
