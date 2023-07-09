ff035ae2eb1966182b4ebe1d0aa0330351edfee4
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-07-09 11:31:32 +0100

    add wheel back to package

--- build_meteortools.ps1
+++ build_meteortools.ps1
@@ -11,4 +11,5 @@ if ($LASTEXITCODE -eq 0 ) {
     $prod = read-host -prompt "(T)est or (P)rod?"
     if ( $prod.ToUpper() -eq "P") {$repo = "pypi"} else {$repo = "pypitest"}
     python -m twine upload --repository $repo dist/*.gz
+    python -m twine upload --repository $repo dist/*.whl
 }
