757a055dd61c1561eb6567ce587a77525d68ff82
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-08-02 16:51:22 +0100

    support for new account

--- archive/terraform/mjmm/variables.tf
+++ archive/terraform/mjmm/variables.tf
@@ -6,3 +6,4 @@ variable "region" { default = "eu-west-2" }
 variable "remote_profile" { default = "ukmonshared"}
 variable "remote_account_id" { default = "822069317839" }
 variable "remote_region" {default = "eu-west-2"}
+variable "mda_account_id" { default = "183798037734" }
