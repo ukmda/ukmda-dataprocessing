7f7180509082ff80135772f818e020e1c37bde96
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-08-05 08:17:09 +0100

    updates to inform user of progress

--- archive/ukmon_pylib/maintenance/purgeOldImages.py
+++ archive/ukmon_pylib/maintenance/purgeOldImages.py
@@ -8,8 +8,8 @@ import sys
 def findImagesToDelete(archbucket, location):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(archbucket)
-    # get list of files to be removed from S3
-    print(f'checking {archbucket}/{location}')
+    print(f'clearing down images from {archbucket}/{location}... ', end='')
+    sys.stdout.flush()
     files = [os.key for os in bucket.objects.filter(Prefix=location)]
     fltfiles = [file for file in files if 'M20' in file or 'FF_' in file]
     imgfiles = [file for file in fltfiles if '.jpg' in file or '.mp4' in file]
@@ -20,7 +20,6 @@ def findImagesToDelete(archbucket, location):
 
 
 def deleteImages(flist, archbucket):
-    print(f'clearing down images from S3 bucket {archbucket}')
     s3 = boto3.client('s3')
     chunk_size = 900
     chunked_list = [flist[i:i + chunk_size] for i in range(0, len(flist), chunk_size)]
@@ -34,8 +33,10 @@ def deleteImages(flist, archbucket):
 def processFolders(archbucket):
     s3 = boto3.client('s3')
     x = s3.list_objects_v2(Bucket=archbucket, Prefix='archive/', Delimiter='/')
+    totalimgs = 0
     for pr in x['CommonPrefixes']: 
-        findImagesToDelete(archbucket, pr['Prefix'])
+        totalimgs += findImagesToDelete(archbucket, pr['Prefix'])
+    print(f'purged {totalimgs} in total')
 
 
 if __name__ == '__main__':
