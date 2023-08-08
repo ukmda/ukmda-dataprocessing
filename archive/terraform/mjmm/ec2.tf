3a1b5e13774211cdda7d6948456ddf0d9ce74528
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-08-08 17:50:45 +0100

    removing unused assets

--- archive/terraform/mjmm/ec2.tf
+++ archive/terraform/mjmm/ec2.tf
@@ -1,56 +1,5 @@
 # Copyright (C) 2018-2023 Mark McIntyre
 
-/*
-resource "aws_instance" "ukmonhelper" {
-  ami                  = "ami-0a669382ea0feb73a"
-  instance_type        = "t3a.micro"
-  iam_instance_profile = aws_iam_instance_profile.S3FullAccess.name
-  key_name             = aws_key_pair.marks_key.key_name
-  security_groups      = [aws_security_group.launch-wizard-4.name]
-
-  root_block_device {
-    tags = {
-      "Name"       = "UKMonHelperVol"
-      "billingtag" = "ukmon"
-      "project"    = "UKMonHelper"
-    }
-    volume_size = 50
-    volume_type = "gp3"
-    throughput = 125
-    iops = 3000
-  }
-
-  tags = {
-    "Name"       = "UKMonHelper"
-    "billingtag" = "ukmon"
-    "project"    = "UKMonHelper"
-  }
-}
-# elastic IP attached to the ukmonhelper server
-resource "aws_eip" "ukmonhelper" {
-  instance = aws_instance.ukmonhelper.id
-  vpc      = true
-  tags = {
-    billingtag = "ukmon"
-  }
-}
-# elastic network interface attached to the ukmonhelper server
-resource "aws_network_interface" "ukmon_if" {
-  subnet_id                 = aws_subnet.ec2Subnet.id
-  private_ips               = ["172.31.12.116"]
-  security_groups           = [aws_security_group.launch-wizard-4.id]
-  ipv6_address_list_enabled = false
-  tags = {
-    "Name"       = "ukmonhelper_if"
-    "billingtag" = "ukmon"
-  }
-  attachment {
-    instance     = aws_instance.ukmonhelper.id
-    device_index = 0
-  }
-}
-*/
-
 
 resource "aws_instance" "ukmonhelper_g" {
   ami                  = "ami-0c127ddea5a07804b"
