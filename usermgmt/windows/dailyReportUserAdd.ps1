8e83c0bab15716efa65ac4bea0fdb045355c9c2b
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-09-26 11:09:19 +0100

    daily report no longer sent by email so no need to maintain the list

--- usermgmt/windows/dailyReportUserAdd.ps1
+++
@@ -1,9 +0,0 @@
-# powershell script to add user to dailyReport email
-# Copyright (C) 2018-2023 Mark McIntyre
-
-set-location $psscriptroot
-aws s3 cp s3://ukmon-shared/admin/dailyReportRecips.txt ./caminfo/ --profile ukmon-markmcintyre
-cmd /c start /wait "C:\Program Files\TextPad 8\TextPad.exe" "caminfo\dailyReportRecips.txt"
-Write-Output "uploading"
-pause
-aws s3 cp .\caminfo\dailyReportRecips.txt s3://ukmon-shared/admin/  --profile ukmon-markmcintyre
