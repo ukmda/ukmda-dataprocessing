badffce0075b3267dd39fb06083501399e7ddec5
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-09-29 22:05:03 +0100

    remove unused artefacts from EE account

--- archive/terraform/ee/ecr_artefacts.tf
+++ archive/terraform/ee/ecr_artefacts.tf
@@ -26,45 +26,8 @@ resource "aws_ecr_repository" "extrafilesrepo" {
   }
 }
 
-# create an ECR repository for FTPtoUKMON
-resource "aws_ecr_repository" "ftptoukmonrepo" {
-  name                 = "ukmon/ftptoukmon"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-  encryption_configuration {
-    encryption_type = "KMS"
-    kms_key         = aws_kms_key.container_key.arn
-  }
-  tags = {
-    "billingtag" = "ukmon"
-  }
-}
 resource "aws_ecr_lifecycle_policy" "extrafilespolicy" {
   repository = aws_ecr_repository.extrafilesrepo.name
-  policy     = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-                        "description": "Keep only latest two versions images",
-            "selection": {
-                "tagStatus": "any",
-                "countType": "imageCountMoreThan",
-                "countNumber": 2
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_ecr_lifecycle_policy" "ftpyoukmonpolicy" {
-  repository = aws_ecr_repository.ftptoukmonrepo.name
   policy     = <<EOF
 {
     "rules": [
