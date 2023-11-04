57d2ca5107854ad38a14a61fa1788f82ba80a98b
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-11-04 12:57:38 +0000

    remove unused triggers

--- archive/terraform/ukmda/ukmdashared_triggers.tf
+++ archive/terraform/ukmda/ukmdashared_triggers.tf
@@ -33,12 +33,12 @@ resource "aws_s3_bucket_notification" "ukmdashared_notification" {
     filter_suffix       = ".txt"
   }
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.imgreplicatelambda.arn
-    id                  = "synctoukmon"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "archive/"
-  }
+  #lambda_function {
+  #  lambda_function_arn = aws_lambda_function.imgreplicatelambda.arn
+  #  id                  = "synctoukmon"
+  #  events              = ["s3:ObjectCreated:*"]
+  #  filter_prefix       = "archive/"
+  #}
 
 }
 
@@ -60,11 +60,11 @@ resource "aws_lambda_permission" "permftptoukmdalambda" {
   source_arn     = aws_s3_bucket.ukmdashared.arn
 }
 
-resource "aws_lambda_permission" "permarcreplicatelambda" {
-  statement_id   = "AllowExecutionFromShrBucket"
-  action         = "lambda:InvokeFunction"
-  function_name  = aws_lambda_function.imgreplicatelambda.arn
-  principal      = "s3.amazonaws.com"
-  source_account = data.aws_caller_identity.current.account_id
-  source_arn     = aws_s3_bucket.ukmdashared.arn
-}
+#resource "aws_lambda_permission" "permarcreplicatelambda" {
+#  statement_id   = "AllowExecutionFromShrBucket"
+#  action         = "lambda:InvokeFunction"
+#  function_name  = aws_lambda_function.imgreplicatelambda.arn
+#  principal      = "s3.amazonaws.com"
+#  source_account = data.aws_caller_identity.current.account_id
+#  source_arn     = aws_s3_bucket.ukmdashared.arn
+#}
