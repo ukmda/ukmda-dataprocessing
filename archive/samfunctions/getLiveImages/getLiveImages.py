74d3cd6a04d1ef963ca85701821482c0a827d21e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-11-09 00:00:13 +0000

    support for new livestream

--- archive/samfunctions/getLiveImages/getLiveImages.py
+++ archive/samfunctions/getLiveImages/getLiveImages.py
@@ -5,6 +5,7 @@
 import boto3
 from boto3.dynamodb.conditions import Key
 import json
+import datetime
 
 
 def getLiveImages(dtstr):
@@ -16,19 +17,53 @@ def getLiveImages(dtstr):
     return resp
 
 
+def getImageUrls(dtstr, statid, token=None, maxitems=100):
+    s3 = boto3.client('s3')
+    buckname = 'ukmda-live'
+    if token is None:
+        resp = s3.list_objects_v2(Bucket=buckname, MaxKeys = maxitems*2, Prefix=f'M{dtstr}')
+    else:
+        resp = s3.list_objects_v2(Bucket=buckname, MaxKeys = maxitems*2, ContinuationToken = token, Prefix=f'M{dtstr}')
+    urls = []
+    if resp['KeyCount'] > 0:
+        for key in resp['Contents']:
+            keyval = key['Key']
+            if '.jpg' in keyval:
+                psurl = s3.generate_presigned_url(ClientMethod='get_object',Params={'Bucket': buckname,'Key': keyval}, ExpiresIn=1800)
+                urls.append({'url': f'{psurl}'})
+        if 'NextContinuationToken' in resp:
+            token = resp['NextContinuationToken']
+    retval = {'pagetoken': token, 'urls': urls}
+    return retval
+
+
 def lambda_handler(event, context):
     #print(event)
     qs = event['queryStringParameters']
-    patt = qs['pattern']
-    print(f'searching for {patt}')
-    ecsvstr = getLiveImages(patt)
-    print(f"found {ecsvstr['Items']}")
-    return {
-        'statusCode': 200,
-        'body': json.dumps(ecsvstr['Items'])
-    }
-
-
-if __name__ == '__main__':
-    ecsvstr = getLiveImages('20230225_2101')
-    print(ecsvstr)
+    if 'pattern' in qs:
+        patt = qs['pattern']
+        print(f'searching for {patt}')
+        ecsvstr = getLiveImages(patt)
+        print(f"found {ecsvstr['Items']}")
+        return {
+            'statusCode': 200,
+            'body': json.dumps(ecsvstr['Items'])
+        }
+    else:
+        maxitems = 100
+        if 'dtstr' in qs:
+            dtstr = qs['dtstr'] 
+        else:
+            dtstr = datetime.datetime.now().strftime('M%Y%m%d_%H')
+        statid = None
+        if 'statid' in qs:
+            statid = qs['statid']
+        conttoken = None
+        if 'conttoken' in qs:
+            conttoken = qs['conttoken']
+        print(dtstr, statid, maxitems)
+        retval = getImageUrls(dtstr, statid, conttoken, maxitems)
+        return {
+            'statusCode': 200,
+            'body': "showImages(" + json.dumps(retval) + ")"
+        }
