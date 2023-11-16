2499e70ec881804c5d5cfda52625fe201c2cd1a0
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-11-16 23:44:47 +0000

    removing feeds to the old ukmon buckets

--- archive/terraform/ukmda/ukmdalive_bucket.tf
+++ archive/terraform/ukmda/ukmdalive_bucket.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "ukmdalive" {
   }
   provider = aws.eu-west-1-prov
 }
-
+/*
 resource "aws_s3_bucket_policy" "ukmdalivebp" {
   bucket   = aws_s3_bucket.ukmdalive.id
   provider = aws.eu-west-1-prov
@@ -60,47 +60,8 @@ resource "aws_s3_bucket_policy" "ukmdalivebp" {
     }
   )
 }
-/*
-resource "aws_s3_bucket_acl" "ukmdaliveacl" {
-  bucket   = aws_s3_bucket.ukmdalive.id
-  provider = aws.eu-west-1-prov
-  access_control_policy {
-    owner {
-      id = data.aws_canonical_user_id.current.id
-    }
-
-    grant {
-      permission = "READ"
-      grantee {
-        type = "Group"
-        uri  = "http://acs.amazonaws.com/groups/global/AllUsers"
-      }
-    }
-    grant {
-      permission = "READ_ACP"
-      grantee {
-        type = "Group"
-        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
-      }
-    }
-    grant {
-      permission = "WRITE"
-      grantee {
-        type = "Group"
-        uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
-      }
-    }
-    grant {
-      permission = "FULL_CONTROL"
-      grantee {
-        #display_name = "ukmeteornetwork"
-        id           = "c17d5e7eafb82458a2f2439be92f8dfc85cee141a142c05d1f1bb1d66dfd3f4b"
-        type         = "CanonicalUser"
-      }
-    }
-  }
-}
 */
+
 resource "aws_s3_bucket_lifecycle_configuration" "ukmdalivelcp" {
   bucket   = aws_s3_bucket.ukmdalive.id
   provider = aws.eu-west-1-prov
@@ -119,29 +80,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "ukmdalivelcp" {
     }
   }
 }
-/*
-resource "aws_s3_bucket_cors_configuration" "ukmdalivecors" {
-  bucket   = aws_s3_bucket.ukmdalive.id
-  provider = aws.eu-west-1-prov
-  cors_rule {
-    allowed_headers = [
-      "*",
-      "x-amz-acl",
-    ]
-    allowed_methods = [
-      "GET",
-      "HEAD",
-      "POST",
-      "PUT",
-    ]
-    allowed_origins = [
-      "*",
-    ]
-    expose_headers  = []
-    max_age_seconds = 30000
-  }
-}
-*/
+
 resource "aws_s3_bucket_logging" "ukmdalivelogs" {
   provider = aws.eu-west-1-prov
   bucket = aws_s3_bucket.ukmdalive.id
--- archive/terraform/ukmda/variables.tf
+++ archive/terraform/ukmda/variables.tf
@@ -6,8 +6,10 @@ variable "mmprofile" { default = "default" }
 variable "websitebucket" { default = "ukmda-website" }
 variable "sharedbucket" { default = "ukmda-shared" }
 variable "livebucket" { default = "ukmda-live" }
+variable "adminbucket" { default = "ukmda-admin" }
+
 variable "region" { default = "eu-west-2"}
-variable "liveregion" { default="eu-west-1"}
+variable "liveregion" { default = "eu-west-1"}
 
 #data used by the code in several places
 data "aws_caller_identity" "current" {}
@@ -16,7 +18,7 @@ data "aws_canonical_user_id" "current" {}
 variable "remote_profile" { default = "default"}
 variable "remote_account_id" { default = "317976261112" }
 variable "remote_region" {default = "eu-west-2"}
-variable "eeaccountid" {default = "822069317839" }
+#variable "eeaccountid" {default = "822069317839" }
 
 variable "main_cidr" {default = "172.32.0.0/16" }
 variable mgmt_cidr { default = "172.32.36.0/22" }
