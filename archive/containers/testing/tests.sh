2e84d18c50a39f72cfd5083311b43b4297af7d7d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-21 15:44:17 +0000

    breaking the testing process

--- archive/containers/testing/tests.sh
+++ archive/containers/testing/tests.sh
@@ -4,10 +4,9 @@ echo "Setup tests"
 ping -c1 www.ukmeteors.co.uk
 pip install --upgrade MeteorTools
 pip install pytest pytest-cov
-cd /tmp
 git clone https://github.com/ukmda/ukmda-dataprocessing.git
-cd /tmp/ukmda-dataprocessing/
+cd /ukmda-dataprocessing/
 git checkout $BRANCH
-cd /tmp/ukmda-dataprocessing/archive/ukmon_pylib/
-export PYTHONPATH=/WesternMeteorPyLib:/RMS:/tmp/ukmda-dataprocessing/archive/ukmon_pylib
+cd /ukmda-dataprocessing/archive/ukmon_pylib/
+export PYTHONPATH=/WesternMeteorPyLib:/RMS://ukmda-dataprocessing/archive/ukmon_pylib
 pytest -v ./tests --cov=. --cov-report=term-missing
