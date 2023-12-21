db477664d3485cb785d04e08deab51f1c3ed82fb
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-21 16:36:32 +0000

    still working on test data

--- archive/containers/testing/tests.sh
+++ archive/containers/testing/tests.sh
@@ -4,14 +4,12 @@ echo "Setup tests"
 ping -c1 www.ukmeteors.co.uk
 pip install --upgrade MeteorTools | grep -v 'already satisfied'
 pip install pytest pytest-cov | grep -v 'already satisfied'
-cd ./data
-DATADIR=$(pwd -p)
-cd ..
 git clone https://github.com/ukmda/ukmda-dataprocessing.git
 cd ./ukmda-dataprocessing/
 git checkout $BRANCH
 cd ./archive/ukmon_pylib/
 curdir=$(pwd -P)
+DATADIR=/data
 echo DATADIR is $DATADIR curdir is $curdir
 export PYTHONPATH=/WesternMeteorPyLib:/RMS:${curdir}
 pytest -v ./tests --cov=. --cov-report=term-missing
