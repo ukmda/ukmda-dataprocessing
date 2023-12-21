2e6f181d000359adebdc1542e14a22b7e249737c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-21 16:07:42 +0000

    fixing test script

--- archive/containers/testing/tests.sh
+++ archive/containers/testing/tests.sh
@@ -7,7 +7,7 @@ pip install pytest pytest-cov grep -v 'already satisfied'
 git clone https://github.com/ukmda/ukmda-dataprocessing.git
 cd ./ukmda-dataprocessing/
 git checkout $BRANCH
-cd ./ukmon_pylib/
+cd ./archive/ukmon_pylib/
 curdir=$(pwd -P)
 export PYTHONPATH=/WesternMeteorPyLib:/RMS:${curdir}
 pytest -v ./tests --cov=. --cov-report=term-missing
