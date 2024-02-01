39cab6dcd5124006408e3580c976d43569daa4be
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-02-01 20:32:00 +0000

    add secondary index on the camera table

--- archive/terraform/ukmda/dynamodb_tables.tf
+++ archive/terraform/ukmda/dynamodb_tables.tf
@@ -60,7 +60,15 @@ resource "aws_dynamodb_table" "camera_table" {
     name = "site"
     type = "S"
   }
-
+      global_secondary_index {
+          hash_key           = "site"
+          name               = "site-stationid-index"
+          non_key_attributes = []
+          projection_type    = "ALL"
+          range_key          = "stationid"
+          read_capacity      = 0
+          write_capacity     = 0
+        }
   #ttl {
   #  attribute_name = "ExpiryDate"
   #  enabled        = true
