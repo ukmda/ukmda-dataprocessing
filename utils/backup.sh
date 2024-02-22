22b5ad1a76e845c37dd5d1ad5c1688908d06a813
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-02-22 21:02:31 +0000

    tweaks to improve backup efficiency

--- utils/backup.sh
+++ utils/backup.sh
@@ -5,21 +5,22 @@ cd $here/ukmda-shared
 
 yr=$(date +%Y)
 echo $(date) singlepq
-aws s3 sync s3://ukmda-shared/matches/singlepq ./matches/singlepq
+aws s3 sync s3://ukmda-shared/matches/singlepq ./matches/singlepq --exclude "*" --include "*${yr}*"
 echo $(date) matchedpq
-aws s3 sync s3://ukmda-shared/matches/matchedpq ./matches/matchedpq
+aws s3 sync s3://ukmda-shared/matches/matchedpq ./matches/matchedpq --exclude "*" --include "*${yr}*"
+
 echo $(date) fireballs
+aws s3 sync s3://ukmda-shared/fireballs ./fireballs --exclude "*" --include "${yr}/"
 
-aws s3 sync s3://ukmda-shared/fireballs ./fireballs
 echo $(date) kmls
+aws s3 sync s3://ukmda-shared/kmls ./kmls --quiet
 
-aws s3 sync s3://ukmda-shared/kmls ./kmls
 echo $(date) consolidated
+    aws s3 sync s3://ukmda-shared/consolidated ./consolidated --exclude "*" --include "*${yr}*" --exclude "temp/*" 
 
-aws s3 sync s3://ukmda-shared/consolidated ./consolidated --exclude "temp/*"
 echo $(date) RMSCorrelate
-
-aws s3 sync s3://ukmda-shared/matches/RMSCorrelate ./matches/RMSCorrelate --exclude "*" --include "*${yr}*"
+aws s3 sync s3://ukmda-shared/matches/RMSCorrelate ./matches/RMSCorrelate --exclude "*" --include "*_${yr}*"
 
 echo $(date) done
 
+# note that backupOldData.py is a one-off to collect data scattered in the old Archive area and should not be rerun.
