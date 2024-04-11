cac77a319491fd53d7336b291eb28cc7f43b184f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-04-11 01:42:28 +0100

    remove disused code

--- usermgmt/windows/stationMaint.ps1
+++ usermgmt/windows/stationMaint.ps1
@@ -1,25 +1,12 @@
 # Copyright (C) 2018-2023 Mark McIntyre
-Set-Location $psscriptroot
-
-$env:SRCBUCKET="ukmda-shared"
-$env:LIVEBUCKET="ukmda-live"
-$env:WEBSITEBUCKET="ukmda-website"
-$env:ADM_PROFILE="ukmda_maint"
-$env:SSHKEY="ukmonhelper"
-$env:HELPERIP="3.11.55.160"
-$env:REMOTEDIR="/home/ec2-user/prod/data"
-$env:PLATEPARDIR=""
-
+push-Location $psscriptroot
 
 # create conda env if not aleady there
 $isadm=(conda env list  | select-string "ukmon-admin")
 if ($isadm.count -eq 0) {
     conda env create -n ukmon-admin python=3.8
-    conda activate ukmon-admin
     pip install -r requirements.txt
 }
-else
-{
-    conda activate ukmon-admin
-}
+conda activate ukmon-admin
 python stationMaint2.py
+Pop-Location
