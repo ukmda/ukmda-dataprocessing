1734141a57e2b67a46378bc4470d9874ec108ab5
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-08-16 00:08:44 +0100

    add column to single-station detections data

--- archive/samfunctions/ftpToUkmon/pythoncode/ftpToUkmon1.py
+++ archive/samfunctions/ftpToUkmon/pythoncode/ftpToUkmon1.py
@@ -41,7 +41,7 @@ def writeUkmdaCsv(dir_path, file_name, data):
     with open(os.path.join(dir_path, file_name), 'w') as f:
 
         # Write the header - no
-        #f.write("Ver,Y,M,D,h,m,s,Mag,Dur,Az1,Alt1,Az2,Alt2,Ra1,Dec1,Ra2,Dec2,ID,Long,Lat,Alt,Tz,AngVel,Shwr,Filename,Dtstamp\n")
+        #f.write("Ver,Y,M,D,h,m,s,Mag,Dur,Az1,Alt1,Az2,Alt2,Ra1,Dec1,Ra2,Dec2,ID,Long,Lat,Alt,Tz,AngVel,Shwr,Filename,Dtstamp,status\n")
 
         # Write meteor data to file
         for line in data:
@@ -62,11 +62,12 @@ def writeUkmdaCsv(dir_path, file_name, data):
                 # add 60 seconds on to the datetime
                 dt = tmpdt + datetime.timedelta(seconds = secs)
             dtstamp = dt.timestamp()
+            sts = 'N'
 
-            f.write('{:s},{:4d},{:2d},{:2d},{:2d},{:2d},{:4.2f},{:.2f},{:.3f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:s},{:.6f},{:.6f},{:.1f},{:.1f},{:7f},{:s},{:s},{:.6f}\n'.format(
+            f.write('{:s},{:4d},{:2d},{:2d},{:2d},{:2d},{:4.2f},{:.2f},{:.3f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:.7f},{:s},{:.6f},{:.6f},{:.1f},{:.1f},{:7f},{:s},{:s},{:.6f},{:s}\n'.format(
                 'UM1', dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second + dt.microsecond/1000000, 
                 peak_mag, duration, azim1, alt1, azim2, alt2, ra1, dec1, ra2, dec2, cam_code, lon, lat, 
-                elev, UT_corr, angvel, shwr, fname, dtstamp))
+                elev, UT_corr, angvel, shwr, fname, dtstamp, sts))
 
     s3 = boto3.resource('s3')
     s3bucket = os.getenv('ARCHBUCKET', default='ukmda-shared')
