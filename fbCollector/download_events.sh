a8e7a514cb74fac77674e918e77820302cb8da9f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-10-04 13:00:59 +0100

    bugfix to correct target path for downloads

--- fbCollector/download_events.sh
+++ fbCollector/download_events.sh
@@ -4,25 +4,25 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 dt=$1
 basedir=$(grep basedir $here/config.ini | awk -F= '{print $2}'|tr -d '\r' | sed 's|f:|/mnt/f|g' | sed 's|c:|/mnt/c|g')
-echo $basedir 
+echo $basedir $dt
 sleep 30
 dtns=$dt 
 mkdir -p $basedir/$dtns
-cd $basedir/$dtns
+pushd $basedir/$dtns
 
-rsync -avz gmn.uwo.ca:/home/uk*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-rsync -avz gmn.uwo.ca:/home/be*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-rsync -avz gmn.uwo.ca:/home/ie*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-rsync -avz gmn.uwo.ca:/home/nl*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
+rsync -avz gmn.uwo.ca:/home/uk*/files/event_monitor/*${dt}*.bz2 .
+rsync -avz gmn.uwo.ca:/home/be*/files/event_monitor/*${dt}*.bz2 .
+rsync -avz gmn.uwo.ca:/home/ie*/files/event_monitor/*${dt}*.bz2 .
+rsync -avz gmn.uwo.ca:/home/nl*/files/event_monitor/*${dt}*.bz2 .
 if [ "$2" == "all" ] ; then 
-    rsync -avz gmn.uwo.ca:/home/fr*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/de*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/es*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/ch*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/it*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/cz*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/hr*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
-    rsync -avz gmn.uwo.ca:/home/sk*/files/event_monitor/*${dt}*.bz2 $basedir/$dtns
+    rsync -avz gmn.uwo.ca:/home/fr*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/de*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/es*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/ch*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/it*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/cz*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/hr*/files/event_monitor/*${dt}*.bz2 .
+    rsync -avz gmn.uwo.ca:/home/sk*/files/event_monitor/*${dt}*.bz2 .
 fi
 
 for f in *.bz2 ; do tar -xvf $f  ; done
@@ -65,3 +65,4 @@ mkdir -p ./mp4s
 cp -f */*.jpg ./jpgs
 mv -f ./jpgs/*captured_stack* ./stacks
 cp -f */*.mp4 ./mp4s
+popd
