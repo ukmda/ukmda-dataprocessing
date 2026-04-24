a926e33e974a07e9941bce3c1380873f7e204317
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-24 22:48:32 +0100

    update install script

--- install_or_update.sh
+++ install_or_update.sh
@@ -13,15 +13,18 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 mkdir -p ~/${envname}
 
 cd $here/archive
+git pull
 
+[ -d ~/$envname/data ] && msg="upgrade" || msg="install"
 for loc in analysis ukmon_pylib website cronjobs utils share static_content
 do
-	rsync -avz $loc/ ~/${envname}/$loc
+	rsync -avz --delete $loc/ ~/${envname}/$loc
 	chmod +x ~/${envname}/$loc/*.sh > /dev/null 2>&1
 done
 DATADIR=~/$envname/data
-mkdir -p $DATADIR/{admin,browse,consolidated,costs,dailyreports,distrib,kmls}
+mkdir -p $DATADIR/{admin,browse,consolidated,costs,dailyreports,distrib,kmls,manualuploads}
 mkdir -p $DATADIR/{lastlogs,latest,matched,orbits,reports,searchidx,single,trajdb,videos}
 mkdir -p $DATADIR/browse/{annual,monthly,daily,showers}
 mkdir -p ~/$envname/logs
 
+echo "$msg complete"
