2010c05bc50d6b35840e5402746d752c7a170ab4
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-24 17:50:35 +0100

    more work on deduplication for ukmon

--- archive/utils/cleanupDeletedTrajs.sh
+++ archive/utils/cleanupDeletedTrajs.sh
@@ -14,8 +14,9 @@ cd ${DATADIR}/distrib
 startdt=$(date --date="-$MATCHSTART days" '+%Y%m%d-080000')
 jdt_min=$(python -c "from wmpl.Utils.TrajConversions import datetime2JD;import datetime;print(datetime2JD(datetime.datetime.strptime('$startdt', '%Y%m%d-%H%M%S')))")
 
-echo "checking main storage, website and csvfiles"
+echo "checking main storage and website"
 sqlite3 $DATADIR/distrib/trajectories.db "select traj_file_path from trajectories where status=0 and jdt_ref > ${jdt_min} order by jdt_ref;" | while read traj ; do
+
    # check if there are two trajectories with the same folder - we don't want to delete the active one
    cnt=$(sqlite3 $DATADIR/distrib/trajectories.db "select count(traj_id)  from trajectories where traj_file_path='$traj';")
    moved=0
@@ -35,25 +36,26 @@ sqlite3 $DATADIR/distrib/trajectories.db "select traj_file_path from trajectorie
          aws s3 mv $webloc/$fname $newloc/$fname --quiet
          moved=1
       done 
-
-      trajpth1=${trajdir:34:19}
-      csvname=$(echo $trajpth1 | sed 's/_/-/g')
-      csvloc=${UKMONSHAREDBUCKET}/matches/${yr}/fullcsv
-      newloc=${UKMONSHAREDBUCKET}/matches/duplicates/csvs/${yr}
-      aws s3 ls $csvloc/ | awk -F " " '{print $4}' | grep $csvname | while read fname; do
-         aws s3 mv $csvloc/$fname $newloc/$fname --quiet
-         moved=1
-      done 
-      csvloc=$DATADIR/orbits/${yr}/fullcsv/processed
-      ls -1 $csvloc/ | grep $csvname | while read fname; do
-         aws s3 mv $csvloc/$fname $newloc/$fname --quiet
-         moved=1
-      done 
       [ $moved == 1 ] && echo "moved $trajpth"
    else
       echo skipping $traj
    fi 
 done
+
+echo "checking raw fullcsv data files"
+yr=${startdt:0:4}
+csvloc=matches/${yr}/fullcsv
+newloc=matches/duplicates/csvs
+python -c "from maintenance.dataMaintenance import removeDeletedTrajCsv;removeDeletedTrajCsv('$csvloc', '$newloc')"
+
+# cater for year-end
+newyr=$(date +%Y)
+if [ $newyr != $yr ] ; then 
+   csvloc=matches/${newyr}/fullcsv
+   newloc=matches/duplicates/csvs
+   python -c "from maintenance.dataMaintenance import removeDeletedTrajCsv;removeDeletedTrajCsv('$csvloc', '$newloc')"
+
+fi 
 
 echo "checking consolidated matches"
 lasttraj=$(sqlite3 $DATADIR/distrib/trajectories.db "select traj_file_path from trajectories where status=0 and jdt_ref > ${jdt_min} order by jdt_ref;" | tail -1)
@@ -74,5 +76,5 @@ if [ $? == 0 ] ; then
     aws s3 sync  $DATADIR/searchidx/ $WEBSITEBUCKET/search/indexes/ --exclude "*" --include "*allevents.csv" --quiet 
 fi 
 
-echo "checking Mariadb Database"
-python -c "from maintenance.dataMaintenance import removeDelTrajFromDb;removeDelTrajFromDb()"
+# no need to check the SQL database in mariadb, as it is populated from the CSV file that 
+# we cleaned up above
