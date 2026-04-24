b9b135321b85a446e8a1a6e6bd7496eb7957c4c1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-24 13:08:03 +0100

    temporarily add mariadb deets to ssm in mjmm account

--- archive/terraform/mjmm/ssm_parameters.tf
+++ archive/terraform/mjmm/ssm_parameters.tf
@@ -1,6 +1,45 @@
 # Copyright (C) 2018-2023 Mark McIntyre
 
 # SSM parameters for use in the environment config
+resource "aws_ssm_parameter" "prod_dbhost" {
+    provider = aws.eu-west-1-prov
+    name  = "prod_dbhost"
+  type  = "String"
+  value = "3.11.55.160"
+  tags = {
+    "billingtag" = "ukmda"
+  }
+}
+
+resource "aws_ssm_parameter" "prod_dbname" {
+  provider = aws.eu-west-1-prov
+  name  = "prod_dbname"
+  type  = "String"
+  value = "ukmon"
+  tags = {
+    "billingtag" = "ukmda"
+  }
+}
+
+resource "aws_ssm_parameter" "prod_dbpw" {
+  provider = aws.eu-west-1-prov
+  name  = "prod_dbpw"
+  type  = "SecureString"
+  value = "Batch33mdl"
+  tags = {
+    "billingtag" = "ukmda"
+  }
+}
+
+resource "aws_ssm_parameter" "prod_dbuser" {
+  provider = aws.eu-west-1-prov
+  name  = "prod_dbuser"
+  type  = "String"
+  value = "batch"
+  tags = {
+    "billingtag" = "ukmda"
+  }
+}
 
 resource "aws_ssm_parameter" "prod_websitebucket" {
   name  = "prod_websitebucket"
