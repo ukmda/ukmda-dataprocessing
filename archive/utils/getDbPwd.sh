b283c2f2dea54d49353805fa308d72697ab35905
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-27 21:48:52 +0100

    add script to retrieve db passwords

--- archive/utils/getDbPwd.sh
+++ archive/utils/getDbPwd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copyright (C) 2018- Mark McIntyre
 
-if [ "$1" == "root" ]  then 
+if [ "$1" == "root" ]  
+then 
     passwd=$(aws ssm get-parameters --names prod_rootdbpw --with-decryption --region eu-west-1 | jq .Parameters[0].Value)
 else
     passwd=$(aws ssm get-parameters --names prod_dbpw --with-decryption --region eu-west-1 | jq .Parameters[0].Value)
