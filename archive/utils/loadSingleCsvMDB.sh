7e4a62e39d000e9b1ca37d2f7ebd133f8910983c
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-28 16:45:15 +0100

    typo

--- archive/utils/loadSingleCsvMDB.sh
+++ archive/utils/loadSingleCsvMDB.sh
@@ -18,7 +18,7 @@ echo "loading $csvname into mariadb"
 logger -s -t loadSQL "starting"
 user=batch
 # leading space to prevent being inserted into environment
-passwd=$(aws ssm get-parameters --names prod_dbpw --with-decryption --region eu-west-1 | jq .Parameters[0].Value | sed 's'"//g')
+passwd=$(aws ssm get-parameters --names prod_dbpw --with-decryption --region eu-west-1 | jq .Parameters[0].Value | sed 's/"//g')
 mysql -p${passwd} -u${user} << EOD
 select count(*) from ukmon.singles;
 LOAD DATA LOCAL INFILE '$csvname' 
