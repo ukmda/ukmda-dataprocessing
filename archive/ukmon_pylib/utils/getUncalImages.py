4d1eb921031bf1efae35c7cc36b4a0949ab8fa2b
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-29 19:43:26 +0100

    bugfix

--- archive/ukmon_pylib/utils/getUncalImages.py
+++ archive/ukmon_pylib/utils/getUncalImages.py
@@ -9,13 +9,17 @@ def getUncalibratedImageList(dtstr=None):
     datadir=os.getenv('DATADIR', default=os.path.expanduser('~/prod/data'))
     now = datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y%m%d')
     if dtstr is not None and dtstr != now:
-        logfile = os.path.join(datadir, '..', 'logs', f'matchJob.log.{dtstr}')
+        logfile = os.path.join(datadir, '..', 'logs', f'matchJob.log-{dtstr}')
     else:
         logfile = os.path.join(datadir, '..', 'logs', 'matchJob.log')
-    flines = open(logfile, 'r').readlines()
-    uncal = [f for f in flines if 'not recalibrated' in f]
-    imglist = [x.split('Skipping ')[1].split(',')[0] for x in uncal]
-    with open(os.path.join(datadir, 'single', 'used', f'uncal_{dtstr}.txt'), 'w') as outf:
-        for li in imglist:
-            outf.write(f'{li}\n')
+    if os.path.isfile(logfile):
+        flines = open(logfile, 'r').readlines()
+        uncal = [f for f in flines if 'not recalibrated' in f]
+        imglist = [x.split('Skipping ')[1].split(',')[0] for x in uncal]
+        with open(os.path.join(datadir, 'single', 'used', f'uncal_{dtstr}.txt'), 'w') as outf:
+            for li in imglist:
+                outf.write(f'{li}\n')
+    else:
+        print('file not found', logfile)
+        imglist=[]
     return imglist
