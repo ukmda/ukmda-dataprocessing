41c37c0542cb7c05a247f82c70273e571fce7cd0
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-04-30 12:37:23 +0100

    whoops naive vs tzaware dates

--- archive/ukmon_pylib/metrics/camMetrics.py
+++ archive/ukmon_pylib/metrics/camMetrics.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
         # could be ubuntu (auth.log) or amazon linux style log
         dtstr = spls[0][spls[0].find(':')+1:][:19]
         if ' ' in dtstr:
-            dtval = datetime.datetime.strptime(f'{now.year} {dtstr[:15]}', '%Y %b %d %H:%M:%S')
+            dtval = datetime.datetime.strptime(f'{now.year} {dtstr[:15]}', '%Y %b %d %H:%M:%S').replace(tzinfo=datetime.timezone.utc)
             if dtval > now:
                 dtval = dtval.replace(year=now.year-1, tzinfo=datetime.timezone.utc)
         else:
