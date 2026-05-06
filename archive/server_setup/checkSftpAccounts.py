4fa95d5872036706009067e60d015c008286b221
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-05-06 16:06:23 +0100

    add support script for during server migrations

---
+++ archive/server_setup/checkSftpAccounts.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import datetime
+
+
+data=open('/home/ubuntu/prod/data/reports/stationlogins.txt').readlines()
+camlist = []
+not_live = []
+still_upl = []
+not_upl = []
+
+livedate = datetime.datetime.now() - datetime.timedelta(days=10)
+for li in data:
+	lastup = li[:19]
+	lastlo = li[21:40]
+	loc = li[42:61]
+	gmnid = li[64:73]
+	via = li[76:].strip()
+	camlist.append([lastup, lastlo, loc, gmnid, via])
+
+	if ">" in lastup and ">" in lastlo:
+		not_live.append([loc, gmnid, lastup, lastlo, via])
+
+	elif ">" not in lastup:
+		lastupdt = datetime.datetime.strptime(lastup, '%Y-%m-%dT%H:%M:%S')
+		if lastupdt >= livedate:
+			still_upl.append([loc, gmnid, lastup, lastlo, via])
+		else:
+			if '>' in lastlo:
+				not_live.append([loc, gmnid, lastup, lastlo, via])
+			else:
+				lastlodt = datetime.datetime.strptime(lastlo, '%Y-%m-%dT%H:%M:%S')
+				if lastlodt >= livedate:
+					not_upl.append([loc, gmnid, lastup, lastlo, via])
+				else:
+					not_live.append([loc, gmnid, lastup, lastlo, via])
+	else: # '>' not in lastlo
+		lastlodt = datetime.datetime.strptime(lastlo, '%Y-%m-%dT%H:%M:%S')
+		if lastlodt >= livedate:
+			not_upl.append([loc, gmnid, lastup, lastlo, via])
+		else:
+			not_live.append([loc, gmnid, lastup, lastlo, via])
+
+
+		
+
+
+with open('still-live.txt','w') as outf:
+	for cam in still_upl:
+		outf.write(','.join(cam) + '\n')
+
+with open('not_uploading.txt','w') as outf:
+	for cam in not_upl:
+		outf.write(','.join(cam) + '\n')
+
+with open('not_live.txt','w') as outf:
+	for cam in not_live:
+		outf.write(','.join(cam) + '\n')
+		
