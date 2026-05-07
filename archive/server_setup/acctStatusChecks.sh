fc6dbb087d96a19722fb096f40e15fe4fe93cb29
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-05-07 13:29:06 +0100

    tweak to script supporting server migration

--- archive/server_setup/acctStatusChecks.sh
+++ archive/server_setup/acctStatusChecks.sh
@@ -4,7 +4,8 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $here
 
-sudo ls -1 /var/sftp > done.txt
+sudo ls -1 /var/sftp > moved.txt
+grep batchs /home/ubuntu/prod/data/reports/stationlogins.txt |awk '{print $3}'| sort > switched.txt
 grep ukmonh /home/ubuntu/prod/data/reports/stationlogins.txt |awk '{print $3}'| sort > pending.txt
 
 ssh ukmonhelper2 "~/prod/server_setup/get-nbd.sh" | while read i ; do echo $i | awk -F"/" '{print $4}' ; done > not-being-done.txt
