ce7b8fdf04c79678226dc377291eee46cbfeea27
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-05-08 13:03:52 +0100

    testing fix for #460

--- archive/ukmon_pylib/reports/getSolutionStati.py
+++ archive/ukmon_pylib/reports/getSolutionStati.py
@@ -92,6 +92,14 @@ def getSolutionStati(fname):
     return totnew, totchk, gotnew, gotchk 
 
 
+def parseDistriblog(logname):
+    lis = open(logname,'r').readlines()
+    dta = [x for x in lis if ('added to fails' in x or ('saved' in x and 'to' in x)) and ('CorrelateEngine' in x or 'CorrelateRMS' in x)]
+    offset = 49 if 'match' in logname else 76
+    for dd in dta:
+        print(dd[offset:].strip())
+
+
 if __name__ == '__main__':
     fname = sys.argv[1]
-    print(getSolutionStati(fname))
+    print(parseDistriblog(fname))
