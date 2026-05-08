4b89c65b8e3436a3d36017875738806f6271698e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-05-08 11:35:53 +0100

    improved reporting on migration

--- archive/server_setup/acctStatusChecks.sh
+++ archive/server_setup/acctStatusChecks.sh
@@ -4,7 +4,7 @@ here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $here
 
-sudo ls -1 /var/sftp > moved.txt
+sudo ls -1 /var/sftp  | egrep -v "test|logup" > moved.txt
 grep batchs /home/ubuntu/prod/data/reports/stationlogins.txt |awk '{print $3}'| sort > switched.txt
 grep ukmonh /home/ubuntu/prod/data/reports/stationlogins.txt |awk '{print $3}'| sort > pending.txt
 
@@ -13,13 +13,15 @@ ssh ukmonhelper2 "~/prod/server_setup/get-all.sh" | while read i ; do echo $i | 
 
 python ~/src/ukmda-dataprocessing/archive/server_setup/checkSftpAccounts.py
 
-echo "Moved:    $(wc -l moved.txt | awk '{print $1}')"
-echo "Switched: $(wc -l switched.txt | awk '{print $1}')"
-echo "Pending:  $(wc -l pending.txt | awk '{print $1}')"
-echo "Total:    $(wc -l still-live.txt | awk '{print $1}')"
-echo ""
-echo "Not Live: $(wc -l not_live.txt | awk '{print $1}')"
-echo "Not Upl:  $(wc -l not_uploading.txt | awk '{print $1}')"
-echo ""
-echo "Dead:     $(wc -l not-being-done.txt | awk '{print $1}')"
-echo ""
+
+echo "Moved:    $(wc -l moved.txt | awk '{print $1}')" > statusreport.txt
+echo "Switched: $(wc -l switched.txt | awk '{print $1}')" >> statusreport.txt
+echo "Pending:  $(wc -l pending.txt | awk '{print $1}')" >> statusreport.txt
+echo "Total:    $(wc -l still-live.txt | awk '{print $1}')" >> statusreport.txt
+echo "" >> statusreport.txt
+echo "Not Live: $(wc -l not_live.txt | awk '{print $1}')" >> statusreport.txt
+echo "Not Upl:  $(wc -l not_uploading.txt | awk '{print $1}')" >> statusreport.txt
+echo "" >> statusreport.txt
+echo "Dead:     $(wc -l not-being-done.txt | awk '{print $1}')" >> statusreport.txt
+echo "" >> statusreport.txt
+cat statusreport.txt
