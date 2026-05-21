a51c88805546fd170f4dea9d0da90e17e5d8d7f1
Mark McIntyre <markmcintyre99@googlemail.com>
2026-05-21 16:27:08 +0100

    another tweak to the script that checks account status

--- archive/server_setup/acctStatusChecks.sh
+++ archive/server_setup/acctStatusChecks.sh
@@ -7,6 +7,14 @@ cd $here
 sudo ls -1 /var/sftp  | egrep -v "test|logup" > moved.txt
 grep batchs /home/ubuntu/prod/data/reports/stationlogins.txt | cut -c 42-61 | sed 's/ //g' | sort | uniq > switched.txt
 grep -v batchs /home/ubuntu/prod/data/reports/stationlogins.txt | cut -c 42-61 | sed 's/ //g' | grep -v StationID| sort | uniq > pending.txt
+rm -f truepending.txt inflight.txt
+cat pending.txt | while read i ; do grep $i withowner.txt > /dev/null ; if [ $? -ne 0 ] ; then echo $i >> truepending.txt ; else echo $i >> inflight.txt ; fi ; done
+rm -f pending.txt
+cat truepending.txt | while read i ; do grep $i /home/ubuntu/prod/data/reports/stationlogins.txt | cut -c43-100  ; done >> pending.txt
+rm -f truepending.txt owneraware.txt
+cat inflight.txt | while read i; do grep $i withowner.txt | awk -F "," '{print $1 $2 $4}' >> owneraware.txt ; done
+rm inflight.txt
+
 
 ssh ukmonhelper2 "~/server_setup/get-nbd.sh" | while read i ; do echo $i | awk -F"/" '{print $4}' ; done > dead.txt
 ssh ukmonhelper2 "~/server_setup/get-all.sh" | while read i ; do echo $i | awk -F"/" '{print $4}' ; done > all-accounts.txt
@@ -16,11 +24,13 @@ python ~/src/ukmda-dataprocessing/archive/server_setup/checkSftpAccounts.py
 
 echo "" > statusreport.txt
 echo "Migrated is a count of how many stations got set up on the new server," >> statusreport.txt
-echo "Done is how many have cut over, pending is how many have not" >> statusreport.txt
+echo "Done is how many have cut over, In Flight is how many are known problems." >> statusreport.txt
+echo "Pending is how many are pending cutover but no response from owner." >> statusreport.txt
 echo "" >> statusreport.txt
-echo "Migrated: $(wc -l moved.txt | awk '{print $1}')" >> statusreport.txt
-echo "Done:     $(wc -l switched.txt | awk '{print $1}')" >> statusreport.txt
-echo "Pending:  $(wc -l pending.txt | awk '{print $1}')" >> statusreport.txt
+echo "Migrated:  $(wc -l moved.txt | awk '{print $1}')" >> statusreport.txt
+echo "Done:      $(wc -l switched.txt | awk '{print $1}')" >> statusreport.txt
+echo "In Flight: $(wc -l owneraware.txt)| awk '{print $1}')" >> statusreport.txt
+echo "Pending:   $(wc -l pending.txt | awk '{print $1}')" >> statusreport.txt
 echo "" >> statusreport.txt
 echo "Live is how many have connected in the last 10 days" >> statusreport.txt
 echo "Inactive is how many didn't connect for 10 days" >> statusreport.txt
@@ -37,4 +47,10 @@ echo "they've left the network or been offline for years" >> statusreport.txt
 echo "" >> statusreport.txt
 echo "Dead:     $(wc -l dead.txt | awk '{print $1}')" >> statusreport.txt
 echo "" >> statusreport.txt
+echo "Pending List">> statusreport.txt
+cat pending.txt >> statusreport.txt
+echo "" >> statusreport.txt
+echo "Not Uploading List">> statusreport.txt
+cat not_uploading.txt >> statusreport.txt
+
 cat statusreport.txt
