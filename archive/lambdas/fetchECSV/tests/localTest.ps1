d56bf9a0d85591a55d3b86e9c21c172d1df8e335
Mark McIntyre <markmcintyre99@googlemail.com>
2026-07-13 11:17:34 +0100

    adding tests for fetchECSV

--- archive/lambdas/fetchECSV/tests/localTest.ps1
+++ archive/lambdas/fetchECSV/tests/localTest.ps1
@@ -1,3 +1,6 @@
 # Copyright (C) 2018-2023 Mark McIntyre
+$here=$PSScriptRoot
 # sam build --profile ukmonshared --region eu-west-1
-sam local invoke --profile ukmonshared -e $args[0]  --region eu-west-1
+sam local invoke --profile ukmonshared -e $here/test.json  --region eu-west-1 > $here/new_output.txt
+windiff $here/expected_output.txt $here/new_output.txt
+
