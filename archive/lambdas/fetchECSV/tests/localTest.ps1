26fb43111c36a4441743a320efc582c971e48f4f
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-09-01 17:25:34 +0100

    null changes picked up probably due to change in datestamps

--- archive/lambdas/fetchECSV/tests/localTest.ps1
+++ archive/lambdas/fetchECSV/tests/localTest.ps1
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2023 Mark McIntyre
+# Copyright (C) 2018- Mark McIntyre
 $here=$PSScriptRoot
 # sam build --profile ukmonshared --region eu-west-1
 sam local invoke --profile ukmonshared -e $here/test.json  --region eu-west-1 > $here/new_output.txt
